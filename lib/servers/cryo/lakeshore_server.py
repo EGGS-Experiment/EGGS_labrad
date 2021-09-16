70387d403cc890af36885d2f0780958574fc1223
Clayton Ho <claytonho@g.ucla.edu>
2021-09-16 00:52:02 -0700

    finished lakeshore, permanent fix

--- lib/servers/cryo/lakeshore_server.py
+++ lib/servers/cryo/lakeshore_server.py
@@ -18,13 +18,11 @@ timeout = 20
 
 from __future__ import absolute_import
 from twisted.internet.defer import inlineCallbacks, returnValue
-from common.lib.servers.serialdeviceserver import SerialDeviceServer
-#from common.lib.servers.serialdeviceserver import SerialDeviceServer, setting, inlineCallbacks, SerialDeviceError, SerialConnectionError, PortRegError
+from common.lib.servers.serialdeviceserver import SerialDeviceServer, setting, inlineCallbacks, SerialDeviceError, SerialConnectionError, PortRegError
 from labrad.server import setting
 from labrad.support import getNodeName
 
-from serial import Serial
-import serial #workaround temporary
+from serial import PARITY_ODD
 
 import numpy as np
 import time #tmp
@@ -33,21 +31,40 @@ SERVERNAME = 'lakeshore336Server'
 TIMEOUT = 1.0
 BAUDRATE = 57600
 BYTESIZE = 7
-PARITY = serial.PARITY_ODD #0 is odd parity
+PARITY = PARITY_ODD #0 is odd parity
 STOPBITS = 1
 CHANNELS = ['A', 'B', 'C', 'D', '0']
 
 class Lakeshore336Server(SerialDeviceServer):
     name = 'Lakeshore336Server'
     regKey = 'Lakeshore336Server'
-    #port = None
     serNode = getNodeName()
 
-    #@inlineCallbacks
-    def initServer(self):
-        #temp workaround since serial server is a pos
-        self.ser = Serial(port = 'COM24', baudrate = BAUDRATE, bytesize = BYTESIZE, parity = PARITY, stopbits = STOPBITS)
-        self.ser.timeout = TIMEOUT
+    # def initServer(self):
+    #     #temp workaround since serial server is a pos
+    #     self.ser = Serial(port = 'COM24', baudrate = BAUDRATE, bytesize = BYTESIZE, parity = PARITY, stopbits = STOPBITS)
+    #     self.ser.timeout = TIMEOUT
+
+    @inlineCallbacks
+    def initServer( self ):
+        # if not self.regKey or not self.serNode: raise SerialDeviceError( 'Must define regKey and serNode attributes' )
+        # port = yield self.getPortFromReg( self.regKey )
+        port = 'COM24'
+        self.port = port
+        #self.timeout = TIMEOUT
+        try:
+            serStr = yield self.findSerial( self.serNode )
+            print(serStr)
+            self.initSerial( serStr, port, baudrate = BAUDRATE, bytesize = BYTESIZE, parity = PARITY, stopbits = STOPBITS)
+        except SerialConnectionError, e:
+            self.ser = None
+            if e.code == 0:
+                print 'Could not find serial server for node: %s' % self.serNode
+                print 'Please start correct serial server'
+            elif e.code == 1:
+                print 'Error opening serial connection'
+                print 'Check set up and restart serial server'
+            else: raise
 
     # READ TEMPERATURE
     @setting(111,'read_temperature', channel = 's', returns='*1v')
@@ -62,8 +79,8 @@ class Lakeshore336Server(SerialDeviceServer):
         if channel not in CHANNELS:
             raise Exception('Channel must be one of: ' + str(CHANNELS))
         yield self.ser.write('KRDG? ' + str(channel) + '\r\n')
-        resp = yield self.ser.read_until()
-        resp = resp[:-2]
+        time.sleep(0.1)
+        resp = yield self.ser.read()
         resp = np.array(resp.split(','), dtype=float)
         returnValue(resp)
 
