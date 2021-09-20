f1d5a60bd7b809b0807fb7711dd03dc0218f72e0
Clayton Ho <claytonho@g.ucla.edu>
2021-09-20 13:51:22 -0700

    fixed VibrationMeasurement experiment

--- lib/experiments/vibration_measurement.py
+++ lib/experiments/vibration_measurement.py
@@ -52,7 +52,7 @@ class vibration_measurement(experiment):
                 break
 
             # pressure = self.pump.read_pressure()
-            tempK = self.tempcontroller.read_temperature('0')
+            tmp tempK = self.tempcontroller.read_temperature('0')
             # trace = self.oscope.get_trace('1')
 
             try:
@@ -75,9 +75,11 @@ class vibration_measurement(experiment):
 
         #create datasets
         self.dv.cd(['',year,month,trunk], True, context = self.c_result)
-        dataset_temp = self.dv.new('Lakeshore 336 Temperature Controller',[('time', 't')], [('Temperature Diode 1', 'Temperature', 'K')],\
-                                   [('Temperature Diode 2', 'Temperature', 'K')], [('Temperature Diode 3', 'Temperature', 'K')], \
-                                   [('Temperature Diode 4', 'Temperature', 'K')], context = self.c_result)
+        dataset_temp = self.dv.new('Lakeshore 336 Temperature Controller',[('time', 't')], [\
+                                    ('Temperature Diode 1', 'Temperature', 'K'),\
+                                    ('Temperature Diode 2', 'Temperature', 'K'),\
+                                    ('Temperature Diode 3', 'Temperature', 'K'), \
+                                    ('Temperature Diode 4', 'Temperature', 'K')], context = self.c_result)
         #dataset_pressure = self.dv.new('TwisTorr 74 Pressure Controller',[('time', 't')], [('Pump Pressure', 'Pressure', 'mTorr')], context = self.c_result)
         #dataset_oscope = self.dv.new('Rigol DS1104z Oscilloscope',[('time', 't')], [('Scope Trace', 'Scope Trace', '1')], context = self.c_result)
 
