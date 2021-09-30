cecf395071c75db9d96320df4c9ef91d22ac6af6
Clayton Ho <claytonho@g.ucla.edu>
2021-09-30 03:05:56 -0700

    further vibrationmeasurement bugfixes

--- lib/experiments/vibration_measurement.py
+++ lib/experiments/vibration_measurement.py
@@ -41,12 +41,10 @@ class vibration_measurement(experiment):
         self.time_interval = 1.0
 
         #convert parameter to labrad type
+        #welp, todo
 
         #dataset context
-        self.c_temp_1 = self.cxn.context()
-        self.c_temp_2 = self.cxn.context()
-        self.c_temp_3 = self.cxn.context()
-        self.c_temp_4 = self.cxn.context()
+        self.c_temp = self.cxn.context()
 
         #set up data vault
         self.set_up_datavault()
@@ -65,13 +63,9 @@ class vibration_measurement(experiment):
             #tempK = self.tempcontroller.read_temperature('0')
             tempK = np.array([0.1,0.2,0.3,0.4],dtype=float)
             # trace = self.oscope.get_trace('1')
-            #print(tempK)
             elapsedtime = time.time() - starttime
             try:
-                self.dv.add(elapsedtime, tempK[0], context = self.c_temp_1)
-                self.dv.add(elapsedtime, tempK[1], context = self.c_temp_2)
-                self.dv.add(elapsedtime, tempK[2], context = self.c_temp_3)
-                self.dv.add(elapsedtime, tempK[3], context = self.c_temp_4)
+                self.dv.add(elapsedtime, tempK[0], tempK[1], tempK[2], tempK[3], context = self.c_temp)
             except:
                 pass
 
@@ -89,27 +83,19 @@ class vibration_measurement(experiment):
         hour  = '%02d' % date.hour      # Padded with a zero if one digit
         minute = '%02d' % date.minute   # Padded with a zero if one digit
 
-        trunk = year + '_' + month + '_' + day
+        trunk1 = year + '_' + month + '_' + day
         trunk2 = self.name + '_' + hour + ':' + minute
 
         #create datasets
-        self.dv.cd(['',year,month,trunk,trunk2], True, context = self.c_temp_1)
-        dataset_temp1 = self.dv.new('Lakeshore 336 Temperature Controller',[('time', 't')], [('Temperature Diode 1', 'Temperature', 'K')], context = self.c_temp_1)
-        self.dv.cd(['', year, month, trunk,trunk2], True, context=self.c_temp_2)
-        dataset_temp2 = self.dv.new('Lakeshore 336 Temperature Controller',[('time', 't')], [('Temperature Diode 2', 'Temperature', 'K')], context = self.c_temp_2)
-        self.dv.cd(['', year, month, trunk,trunk2], True, context=self.c_temp_3)
-        dataset_temp = self.dv.new('Lakeshore 336 Temperature Controller', [('time', 't')], [('Temperature Diode 3', 'Temperature', 'K')], context = self.c_temp_3)
-        self.dv.cd(['', year, month, trunk,trunk2], True, context=self.c_temp_4)
-        dataset_temp = self.dv.new('Lakeshore 336 Temperature Controller', [('time', 't')], [('Temperature Diode 4', 'Temperature', 'K')], context = self.c_temp_4)
+        self.dv.cd(['', year, month, trunk1, trunk2], True, context = self.c_temp)
+        dataset_temp = self.dv.new('Lakeshore 336 Temperature Controller',[('time', 't')], [('Diode 1', 'Temperature', 'K'), ('Diode 2', 'Temperature', 'K'), \
+                                                                                             ('Diode 3', 'Temperature', 'K'), ('Diode 4', 'Temperature', 'K')] , context = self.c_temp)
         #dataset_pressure = self.dv.new('TwisTorr 74 Pressure Controller',[('time', 't')], [('Pump Pressure', 'Pressure', 'mTorr')], context = self.c_result)
         #dataset_oscope = self.dv.new('Rigol DS1104z Oscilloscope',[('time', 't')], [('Scope Trace', 'Scope Trace', '1')], context = self.c_result)
 
         #add parameters to data vault
         for parameter in self.p:
-            self.dv.add_parameter(parameter, self.p[parameter], context = self.c_temp_1)
-            self.dv.add_parameter(parameter, self.p[parameter], context=self.c_temp_2)
-            self.dv.add_parameter(parameter, self.p[parameter], context=self.c_temp_3)
-            self.dv.add_parameter(parameter, self.p[parameter], context=self.c_temp_4)
+            self.dv.add_parameter(parameter, self.p[parameter], context = self.c_temp)
 
         #set live plotting
         #self.grapher.plot(dataset, 'bright/dark', False)
