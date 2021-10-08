0007ad98182143b68546a2cbf98b832728dbaae2
Clayton Ho <claytonho@g.ucla.edu>
2021-10-07 21:01:04 -0700

    temp save

--- lib/clients/lakeshore_client/lakeshore_gui.py
+++ lib/clients/lakeshore_client/lakeshore_gui.py
@@ -1,4 +1,5 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QFont
 import sys
 
 from common.lib.clients.qtui.q_custom_text_changing_button import TextChangingButton as _TextChangingButton
@@ -21,28 +22,30 @@ class lakeshore_gui(QtWidgets.QFrame):
 
         #temperature readout
         self.tempAll_label = QtWidgets.QLabel('Temperature Readout')
+        self.tempAll_label.setFont(QFont(shell_font, pointSize= 17))
+        self.tempAll_label.setAlignment(QtCore.Qt.AlignCenter)
             #diode 1
         self.temp1_label = QtWidgets.QLabel('Diode 1')
         self.temp1 = QtWidgets.QLabel('Diode 1')
-        self.temp1.setFont(QtGui.QFont(shell_font, pointSize=25))
+        self.temp1.setFont(QFont(shell_font, pointSize=25))
         self.temp1.setAlignment(QtCore.Qt.AlignCenter)
         self.temp1.setStyleSheet('color: blue')
             #diode 2
         self.temp2_label = QtWidgets.QLabel('Diode 2')
         self.temp2 = QtWidgets.QLabel('Diode 2')
-        self.temp2.setFont(QtGui.QFont(shell_font, pointSize=25))
+        self.temp2.setFont(QFont(shell_font, pointSize=25))
         self.temp2.setAlignment(QtCore.Qt.AlignCenter)
         self.temp2.setStyleSheet('color: blue')
             #diode 3
         self.temp3_label = QtWidgets.QLabel('Diode 3')
         self.temp3 = QtWidgets.QLabel('Diode 3')
-        self.temp3.setFont(QtGui.QFont(shell_font, pointSize=25))
+        self.temp3.setFont(QFont(shell_font, pointSize=25))
         self.temp3.setAlignment(QtCore.Qt.AlignCenter)
         self.temp3.setStyleSheet('color: blue')
             #diode 4
         self.temp4_label = QtWidgets.QLabel('Diode 4')
         self.temp4 = QtWidgets.QLabel('Diode 4')
-        self.temp4.setFont(QtGui.QFont(shell_font, pointSize=25))
+        self.temp4.setFont(QFont(shell_font, pointSize=25))
         self.temp4.setAlignment(QtCore.Qt.AlignCenter)
         self.temp4.setStyleSheet('color: blue')
             #record button
@@ -50,11 +53,13 @@ class lakeshore_gui(QtWidgets.QFrame):
 
         #heaters
         self.heatAll_label = QtWidgets.QLabel('Heater Configuration')
+        self.heatAll_label.setFont(QFont(shell_font, pointSize= 17))
+        self.heatAll_label.setAlignment(QtCore.Qt.AlignCenter)
         self.lockswitch = TextChangingButton(('Locked', 'Unlocked'))
             #heater 1
         self.heat1_label = QtWidgets.QLabel('Heater 1')
         self.heat1 = QtWidgets.QLabel('Current 1')
-        self.heat1.setFont(QtGui.QFont(shell_font, pointSize=25))
+        self.heat1.setFont(QFont(shell_font, pointSize=25))
         self.heat1.setAlignment(QtCore.Qt.AlignCenter)
         self.heat1.setStyleSheet('color: red')
         self.heat1_toggle = TextChangingButton(('On', 'Off'))
@@ -76,7 +81,7 @@ class lakeshore_gui(QtWidgets.QFrame):
         self.heat1_res.addItem('25')
         self.heat1_res.addItem('50')
                 #max current
-        self.spinGain = QDoubleSpinBox()
+        self.spinGain = QtWidgets.QDoubleSpinBox()
         self.spinGain.setFont(QFont(shell_font, pointSize=16))
         self.spinGain.setDecimals(3)
         self.spinGain.setSingleStep(1e-3)
@@ -97,7 +102,7 @@ class lakeshore_gui(QtWidgets.QFrame):
         # highRail.setAlignment(QtCore.Qt.AlignCenter)
         # # dac voltage  label
         # setDacVoltage = QLabel(' Set Dac Voltage')
-        # setDacVoltage.setFont(QtGui.QFont(shell_font, pointSize=16))
+        # setDacVoltage.setFont(QFont(shell_font, pointSize=16))
         # setDacVoltage.setAlignment(QtCore.Qt.AlignCenter)
         # # clear lock button for voltage stuck too high
         # self.clear_lock = QPushButton('Clear Lock Voltage')
@@ -105,18 +110,17 @@ class lakeshore_gui(QtWidgets.QFrame):
         # self.clear_lock.setFont(QFont('MS Shell Dlg 2', pointSize=12))
 
         layout.addWidget(self.tempAll_label, 1, 3)
-        layout.addWidget(self.temp1, 2, 2, 3, 2)
-        layout.addWidget(self.temp2, 7, 2, 3, 2)
-        layout.addWidget(self.temp3, 12, 2, 3, 2)
-        layout.addWidget(self.temp4, 17, 2, 3, 2)
+        layout.addWidget(self.temp1, 3, 2, 3, 2)
+        layout.addWidget(self.temp2, 8, 2, 3, 2)
+        layout.addWidget(self.temp3, 13, 2, 3, 2)
+        layout.addWidget(self.temp4, 18, 2, 3, 2)
 
-        layout.addWidget(self.temp1_label, 1, 2)
-        layout.addWidget(self.temp2_label, 6, 2)
-        layout.addWidget(self.temp3_label, 11, 2)
-        layout.addWidget(self.temp4_label, 16, 2)
+        layout.addWidget(self.temp1_label, 2, 2)
+        layout.addWidget(self.temp2_label, 7, 2)
+        layout.addWidget(self.temp3_label, 12, 2)
+        layout.addWidget(self.temp4_label, 17, 2)
 
-        layout.addWidget(self.lockswitch, 1, 3, 1, 1)
-        layout.addWidget(self.heat)
+        layout.addWidget(self.heatAll_label, 1, 10)
         # layout.addWidget(gainName, 2, 3, 1, 1)
         # layout.addWidget(self.spinGain, 3, 3, 1, 1)
         # layout.addWidget(lowRail, 4, 3, 1, 1)
@@ -134,7 +138,7 @@ class lakeshore_gui(QtWidgets.QFrame):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    icon = lakeshore_gui('Lakeshore 336 Temperature Controller')
+    icon = lakeshore_gui()
     icon.show()
     app.exec_()
 
