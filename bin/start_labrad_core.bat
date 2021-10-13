917fe9afe96c2a15029f4912c0ea00b8511cfc1d
Clayton Ho <claytonho@g.ucla.edu>
2021-10-13 01:32:53 -0700

    minor updates

--- bin/start_labrad_core.bat
+++ bin/start_labrad_core.bat
@@ -3,4 +3,4 @@
 start "Labrad Manager" /min %HOME%/Code/scalabrad-0.8.3/bin/labrad.bat --tls-required false
 start "Labrad Web Manager" /min %HOME%/Code/scalabrad-web-server-2.0.6/bin/labrad-web.bat
 start "Labrad Node" /min cmd "/k activate labart && python %HOME%/Code/pylabrad/labrad/node/__init__.py"
-start "" "%ProgramFiles(x86)%\chrome-win\chrome.exe" http://%LABRAD_HOST%:7667
+start "" "%ProgramFiles(x86)%\chrome-win\chrome.exe" http://%LABRADHOST%:7667
