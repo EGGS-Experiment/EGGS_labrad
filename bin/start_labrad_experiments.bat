2b0e914d608c23ac8a1f573a5b65eaf77b5919ed
Clayton Ho <claytonho@g.ucla.edu>
2021-10-14 23:23:02 -0700

    updated bat files, added start_labrad_node bat file

--- bin/start_labrad_experiments.bat
+++ bin/start_labrad_experiments.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
 
-::start "Labrad Data Vault" /min cmd "/k activate barium && python %HOME%/Code/servers/data_vault.py"
 start "Labrad Data Vault" /min cmd "/k activate labart && python %HOME%/Code/EGGS_labrad/lib/servers/data_vault.py"
 start "Labrad Parameter Vault" /min cmd "/k activate labart && python %HOME%/Code/EGGS_labrad/lib/servers/parameter_vault/parameter_vault.py"
 start "Labrad Script Scanner" /min cmd "/k activate labart && python %HOME%/Code/EGGS_labrad/lib/servers/script_scanner/script_scanner.py"
--- bin/start_labrad_node.bat
+++ bin/start_labrad_node.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
 
-start "Labrad Manager" /min %HOME%/Code/scalabrad-0.8.3/bin/labrad.bat --tls-required false
 start "Labrad Web GUI" /min %HOME%/Code/scalabrad-web-server-2.0.6/bin/labrad-web.bat
 start "Labrad Node" /min cmd "/k activate labart && python %HOME%/Code/pylabrad/labrad/node/__init__.py"
 start "" "%ProgramFiles(x86)%\chrome-win\chrome.exe" http://localhost:7667
