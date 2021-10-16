226b82c921759f8a45e9693c8e21a580f37e5ecb
Clayton Ho <claytonho@g.ucla.edu>
2021-10-15 23:38:57 -0700

    updates to niops03 server

--- lib/servers/cryo/niops03_server.py
+++ lib/servers/cryo/niops03_server.py
@@ -24,7 +24,7 @@ from serial import PARITY_ODD
 
 import numpy as np
 
-TIMEOUT = 1.0
+TIMEOUT = 3.0
 BAUDRATE = 115200
 TERMINATOR = '\r\n'
 
@@ -58,22 +58,19 @@ class NIOPS03Server(SerialDeviceServer):
                 raise Exception('Unknown connection error')
 
     #STATUS
-    @setting(11,'Status')
-    def get_status(self, c, power = None):
+    @setting(11,'Status', returns = 's')
+    def get_status(self, c):
         """
-        Set or query whether ion pump is off or on
-        Args:
-            (bool): whether pump is to be on or off
+        Get controller status
         Returns:
-            (float): pump power status
+            (str): which switches and alarms are on/off
         """
-        #create and send message to device
-        self.ser.write('TS' + TERMINATOR)
+        yield self.ser.write('TS' + TERMINATOR)
         resp = yield self.ser.read()
         return resp
 
     # ON/OFF
-    @setting(111,'Toggle IP', power = 'b')
+    @setting(111,'Toggle IP', power = 'b', returns = 's')
     def toggle_ip(self, c, power = None):
         """
         Set or query whether ion pump is off or on
@@ -82,34 +79,13 @@ class NIOPS03Server(SerialDeviceServer):
         Returns:
             (float): pump power status
         """
-        #create and send message to device
         if power == True:
-            self.ser.write('G' + TERMINATOR)
+            yield self.ser.write('G' + TERMINATOR)
         elif power == False:
-            self.ser.write('B' + TERMINATOR)
-
-        resp = yield self.ser.read()
-        return
-
-    @setting(112,'Toggle NP', power = 'b')
-    def toggle_np(self, c, power = None):
-        """
-        Set or query whether getter is off or on
-        Args:
-            (bool): whether getter is to be on or off
-        Returns:
-            (float): getter power status
-        """
-        #create and send message to device
-        if power == True:
-            self.ser.write('GN' + TERMINATOR)
-        elif power == False:
-            self.ser.write('BN' + TERMINATOR)
-
+            yield self.ser.write('B' + TERMINATOR)
         resp = yield self.ser.read()
         return resp
 
-    #
     @setting(112,'Toggle NP', power = 'b')
     def toggle_np(self, c, power = None):
         """
@@ -117,16 +93,62 @@ class NIOPS03Server(SerialDeviceServer):
         Args:
             (bool): whether getter is to be on or off
         Returns:
-            (float): getter power status
+            (bool): getter power status
         """
-        #create and send message to device
         if power == True:
-            self.ser.write('GN' + TERMINATOR)
+            yield self.ser.write('GN' + TERMINATOR)
         elif power == False:
-            self.ser.write('BN' + TERMINATOR)
-
+            yield self.ser.write('BN' + TERMINATOR)
         resp = yield self.ser.read()
-        return
+        return resp
+
+    #PARAMETERS
+    @setting(211,'Get Pressure', returns = 'v')
+    def get_pressure(self, c):
+        """
+        Get ion pump pressure
+        Returns:
+            (float): ion pump pressure
+        """
+        yield self.ser.write('Tb' + TERMINATOR)
+        resp = yield self.ser.read()
+        return float(resp)
+
+    @setting(221,'Get IP Voltage', returns = 'v')
+    def get_pressure(self, c):
+        """
+        Get ion pump voltage
+        Returns:
+            (float): ion pump voltage
+        """
+        yield self.ser.write('Tb' + TERMINATOR)
+        resp = yield self.ser.read()
+        return float(resp)
+
+    @setting(222,'Get NP Voltage', returns = 'v')
+    def get_pressure(self, c):
+        """
+        Get getter voltage
+        Returns:
+            (float): getter voltage
+        """
+        yield self.ser.write('u' + TERMINATOR)
+        resp = yield self.ser.read()
+        return float(resp)
+        #todo: need to convert from hex to decimal, 4 integer values
+
+    @setting(231,'Get Working Time', returns = 'str')
+    def get_pressure(self, c):
+        """
+        Get getter voltage
+        Returns:
+            (float): getter voltage
+        """
+        yield self.ser.write('u' + TERMINATOR)
+        resp = yield self.ser.read()
+        return float(resp)
+        #todo: write correctly
+
 
 if __name__ == '__main__':
     from labrad import util
