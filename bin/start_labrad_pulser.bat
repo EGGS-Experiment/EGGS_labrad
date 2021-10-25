7d423eb46c822cabf04d2daa2f2d05dfcd6c2997
Clayton Ho <claytonho@g.ucla.edu>
2021-10-24 23:38:37 -0700

    created start labrad pulser batch file

--- bin/start_labrad_pulser.bat
+++ bin/start_labrad_pulser.bat
@@ -1,7 +1,11 @@
 @ECHO OFF
 
 start "ARTIQ Master" /min cmd "/k activate labart2 && artiq_master -g -r %ARTIQ_ROOT%/repository --device-db %ARTIQ_ROOT%/device_db.py"
-start "ARTIQ Dashboard" /min cmd "/k activate labart2 && artiq_dashboard"
-start "ARTIQ Controller Manager" /min cmd "/k activate labart2 && artiq_ctlmgr"
+::start "ARTIQ Dashboard" /min cmd "/k activate labart2 && artiq_dashboard"
+::start "ARTIQ Controller Manager" /min cmd "/k activate labart2 && artiq_ctlmgr"
+
+::Submit pulser API to ARTIQ Master to be run
+conda activate labart2 &&
+artiq_client submit %HOME%/EGGS_labrad/lib/servers/pulser2/pulser_api.py
 
 PAUSE
