04bc473fa964bbddb9db44007f685f20e34cb31d
Clayton Ho <claytonho@g.ucla.edu>
2021-10-24 23:19:44 -0700

    added clients to labrad node batch file

--- bin/start_labrad_node.bat
+++ bin/start_labrad_node.bat
@@ -1,7 +1,13 @@
 @ECHO OFF
 
+::Core
 START "Labrad Web GUI" /min %HOME%/Code/scalabrad-web-server-2.0.6/bin/labrad-web.bat
 START "Labrad Node" /min CMD "/k activate labart && python %HOME%/Code/pylabrad/labrad/node/__init__.py"
+START "" "%ProgramFiles(x86)%\chrome-win\chrome.exe" http://localhost:7667
+
+::Device Buses
 START "GPIB Bus Server" /min CMD "/k activate labart && python %HOME%/Code/EGGS_labrad/lib/servers/gpib/gpib_bus_server.py"
 START "Serial Server" /min CMD "/k activate labart && python %HOME%/Code/EGGS_labrad/lib/servers/serial/serial_server.py"
-START "" "%ProgramFiles(x86)%\chrome-win\chrome.exe" http://localhost:7667
+
+::Clients
+START /min CMD /c %HOME%/Code/EGGS_labrad/bin/start_labrad_clients.bat
