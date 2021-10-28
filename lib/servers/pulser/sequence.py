62763c31ad86a733df53b7b6e920da9922cd0379
Clayton Ho <claytonho@g.ucla.edu>
2021-10-27 23:57:24 -0700

    changes to sequence

--- lib/servers/pulser/sequence.py
+++ lib/servers/pulser/sequence.py
@@ -7,23 +7,25 @@ from decimal import Decimal
 
 #todo: use seconds to mu instead of sectostep
 
+from EGGS_labrad.lib.config.hardwareConfiguration import hardwareConfiguration
+
 class Sequence():
     """Sequence for programming pulses"""
     def __init__(self, parent):
         self.channelTotal = hardwareConfiguration.channelTotal
         self.timeResolution = Decimal(hardwareConfiguration.timeResolution)
         self.MAX_SWITCHES = hardwareConfiguration.maxSwitches
-        self.resetstepDuration = hardwareConfiguration.resetstepDuration
+
         #dictionary in the form time:which channels to switch
         #time is expressed as timestep with the given resolution
         #which channels to switch is a channelTotal-long array with 1 to switch ON, -1 to switch OFF, 0 to do nothing
         self.switchingTimes = {0:numpy.zeros(self.channelTotal, dtype = numpy.int8)}
-        self.switches = 1 #keeps track of how many switches are to be performed (same as the number of keys in the switching Times dictionary"
+        self.switches = 1 #keeps track of how many switches are to be performed (same as the number of keys in the switching Times dictionary)
+
         #dictionary for storing information about dds switches, in the format:
         #timestep: {channel_name: integer representing the state}
-            #todo: do another way instead of getcurrentdds, i.e. hardwareconfig
-        self.ddsSettingList = {}.fromkeys(self.parent._getCurrentDDS(), '')
-        self.sectomu=self.parent.sectomu
+        self.ddsSettingList = dict.fromkeys(self.parent.ddsDict.keys(), [])
+        self.sectomu = self.parent.sectomu
 
     #TTL functions
     def addPulse(self, channel, start, duration):
@@ -60,88 +62,24 @@ class Sequence():
         self.ddsSettingList[name].append((start_time, params, start_or_stop))
 
     def parseDDS(self):
-        """
-        fd
-        """
         #sort settings by start time
         for channel_name in self.ddsSettingList.keys():
             self.ddsSettingList[channel_name] = sorted(dds_setting, key=lambda t: t[0])
-        for channel_name, dds_settings in self.ddsSettingList.keys():
-        # check for possible errors
-            # check if pulse endpoints meet
-        if start_time == end_time:
-            # program if new pulse
-            if (end_typ == 'stop') and (new_typ == 'start'):
-                possibleError = (0, '')
-            # don't program if zero-length pulse
-            elif end_typ == 'start' and new_typ == 'stop':
-                possibleError = (0, '')
-                continue
-        # check if pulses overlap
-        elif end_typ == new_typ:
-            possibleError = (start_time, 'Found Overlap Of Two Pulses for channel {}'.format(name))
+        # for channel_name, dds_settings in self.ddsSettingList.keys():
+        # # check for possible errors
+        #     # check if pulse endpoints meet
+        # if start_time == end_time:
+        #     # program if new pulse
+        #     if (end_typ == 'stop') and (new_typ == 'start'):
+        #         possibleError = (0, '')
+        #     # don't program if zero-length pulse
+        #     elif end_typ == 'start' and new_typ == 'stop':
+        #         possibleError = (0, '')
+        #         continue
+        # # check if pulses overlap
+        # elif end_typ == new_typ:
+        #     possibleError = (start_time, 'Found Overlap Of Two Pulses for channel {}'.format(name))
 
-
-    def parseDDS(self):
-        #state holds DDS params for each iteration, then adds it to sequence for each dds
-        state = self.parent._getCurrentDDS()
-
-        # stop if there is no DDS sequence
-        if not bool(len(self.ddsSettingList)):
-            return state
-
-        #keeps track of end time and whether pulse is stopping or starting
-        pulses_end = {}.fromkeys(state, (0, 'stop'))
-        #dds_program holds list of DDS parameters to program
-        dds_program = {}.fromkeys(state, '')
-        #program all sequences
-        last_time_all = 0
-        #get each DDS pulse as (name, time, RAM, on/off)
-        entries = sorted(self.ddsSettingList, key = lambda t: t[1]) #sort by starting time
-        #store errors
-        possibleError = (0, '')
-
-        while True:
-            #get parameters for pulse
-            try:
-                name, start_time, num, new_typ = entries.pop(0)
-                end_time, end_typ = pulses_end[name]
-            except IndexError:
-                if start_time == last_time_all:
-                    #todo: add dds parameters to program
-                    #todo: add timing to switch profiles
-                #at the end of the sequence, reset dds
-                #todo: add timing to switch profiles
-                return dds_program
-
-            #program all DDS pulses
-            if start_time > last_time_all:
-                #raise exception if we have a possible error
-                if (possibleError[0] == lastTime) and (len(possibleError[1]) > 0):
-                    raise Exception(possibleError[1])
-                #todo: add dds parameters to program
-                #move RAM to next position
-                if last_time_all != 0:
-                    #todo: add timing to switch profiles
-                last_time_all = start_time
-
-            #check for possible errors
-                #check if pulse endpoints meet
-            if start_time == end_time:
-                #program if new pulse
-                if (end_typ == 'stop') and (new_typ == 'start'):
-                    possibleError = (0, '')
-                #don't program if zero-length pulse
-                elif end_typ == 'start' and new_typ == 'stop':
-                    possibleError = (0, '')
-                    continue
-            #check if pulses overlap
-            elif end_typ == new_typ:
-                possibleError = (start_time, 'Found Overlap Of Two Pulses for channel {}'.format(name))
-
-            #add pulse
-            state[name] = num
-            pulses_end[name] = (start_time, new_typ)
 
     #Human representation
     def humanRepresentation(self):
