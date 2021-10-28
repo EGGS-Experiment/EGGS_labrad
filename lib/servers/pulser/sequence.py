5e0ff3fe470be79cc2c786cb85a79400928390de
Clayton Ho <claytonho@g.ucla.edu>
2021-10-28 02:14:40 -0700

    tmp

--- lib/servers/pulser/sequence.py
+++ lib/servers/pulser/sequence.py
@@ -1,27 +1,26 @@
-"""
-Used by the pulser server to store a pulse sequence
-"""
-
-import numpy, array
-from decimal import Decimal
+import numpy as np
+import array
 
 #from EGGS_labrad.lib.config.hardwareConfiguration import hardwareConfiguration
 
 class Sequence():
-    """Sequence for programming pulses"""
+    """
+    Sequence for programming pulses.
+    Used by the Pulser server to store a pulse sequence.
+    """
 
     # def __init__(self, parent):
     #     self.channelTotal = hardwareConfiguration.channelTotal
     #     self.MAX_SWITCHES = hardwareConfiguration.maxSwitches
     #
-    #     #dictionary in the form time:which channels to switch
+    #     #dictionary in the form: {time: which channels to switch}
     #     #which channels to switch is a channelTotal-long array with 1 to switch ON, -1 to switch OFF, 0 to do nothing
-    #     self.switchingTimes = {0:numpy.zeros(self.channelTotal, dtype = numpy.int8)}
+    #     self.switchingTimes = {0: np.zeros(self.channelTotal, dtype = np.int8)}
     #     self.switches = 1 #keeps track of how many switches are to be performed (same as the number of keys in the switching Times dictionary)
     #
     #     #dictionary for storing information about dds switches, in the format:
     #     #time: {channel_name: integer representing the state}
-    #     self.ddsSettingList = dict.fromkeys(self.parent.ddsDict.keys(), [])
+    #     self.ddsSettingList = dict.fromkeys(hardwareConfiguration.ddsDict.keys(), [])
     #     self.sectomu = self.parent.sectomu
 
     def __init__(self):
@@ -29,7 +28,7 @@ class Sequence():
         self.timeResolution = 1
         self.MAX_SWITCHES = 1000
 
-        self.switchingTimes = {0:numpy.zeros(self.channelTotal, dtype = numpy.int8)}
+        self.switchingTimes = {0: np.zeros(self.channelTotal, dtype = np.int8)}
         self.switches = 1
 
         self.ddsSettingList = {}
@@ -39,63 +38,23 @@ class Sequence():
     #Sequence functions
     def progRepresentation(self):
         """Returns the programming representation of the sequence"""
-        return self.ddsSettings, self.ttlProgram
+        return self.switchingTimes, self.ddsSettingList
 
     def humanRepresentation(self):
         """Returns the human readable version of the sequence for FPGA for debugging"""
         ttl = self.ttlHumanRepresentation(self.ttlProgram)
-        dds = self.ddsHumanRepresentation(self.ddsSettings)
-        return ttl, dds
-
-    def ddsHumanRepresentation(self, dds):
-        program = []
-        print(dds)
-        for name, buf in dds.items():
-            print("name is ", name)
-            arr = array.array('B', buf)
-            # remove termination
-            arr = arr[:-2]
-            channel = hardwareConfiguration.ddsDict[name]
-            coherent = channel.phase_coherent_model
-            freq_min, freq_max = channel.boardfreqrange
-            ampl_min, ampl_max = channel.boardamplrange
-            #get conversion from binary to values, 65535 = 16 bit max
-            freq_conv = (freq_max - freq_min) / float(65535)
-            ampl_conv = (ampl_max - ampl_min) / float(65535)
-            def chunks(l, n):
-                """ Yield successive n-sized chunks from l."""
-                for i in range(0, len(l), n):
-                    yield l[i:i+n]
-            if coherent:
-                for a0, a1, amp0, amp1, a2, a3, a4, a5, f0, f1, f2, f3, f4, f5, f6, f7, in chunks(arr, 16):
-                    freq_num = (f7 << 24) + (f6 << 16) + (f5 << 8) + f4
-                    ampl_num = (amp1 << 8) + amp0
-                    freq = freq_min + freq_num * freq_conv
-                    print("freq: ", freq)
-                    ampl = ampl_min + ampl_num * ampl_conv
-                    print("ampl: ", ampl)
-                    program.append((name, freq, ampl))
-            else:
-                for a, b, c, d in chunks(arr, 4):
-                    freq_num = (b << 8) + a
-                    ampl_num = (d << 8) + c
-                    freq = freq_min + freq_num * freq_conv
-                    ampl = ampl_min + ampl_num * ampl_conv
-                    program.append((name, freq, ampl))
-        return program
+        return ttl, self.ddsSettingList
 
     def ttlHumanRepresentation(self, rep):
         # recast rep from bytearray into string
         rep = str(rep)
         # does the decoding from the string
-        arr = numpy.fromstring(rep, dtype = numpy.uint16)
-        #arr = numpy.frombuffer(rep, dtype = numpy.uint16)
+        arr = np.fromstring(rep, dtype = np.uint16)
         # once decoded, need to be able to manipulate large numbers
-        arr = numpy.array(arr, dtype = numpy.uint32)
-        #arr = numpy.array(rep,dtype = numpy.uint16)
+        arr = np.array(arr, dtype = np.uint32)
         arr = arr.reshape(-1,4)
-        times = ((arr[:,0] << 16) + arr[:,1]) * float(self.timeResolution)
-        channels = ((arr[:,2] << 16) + arr[:,3])
+        times = ((arr[:, 0] << 16) + arr[:, 1]) * float(self.timeResolution)
+        channels = ((arr[:, 2] << 16) + arr[:, 3])
 
         def expandChannel(ch):
             '''function for getting the binary representation, i.e 2**32 is 1000...0'''
@@ -104,7 +63,7 @@ class Sequence():
             return reverse
 
         channels = map(expandChannel, channels)
-        return numpy.vstack((times, channels)).transpose()
+        return np.vstack((times, channels)).transpose()
 
     #TTL functions
     def addPulse(self, channel, start, duration):
@@ -141,7 +100,7 @@ class Sequence():
                 self.switchingTimes[start_time][chan] = value
         else:
             if self.switches == self.MAX_SWITCHES: raise Exception("Exceeded maximum number of switches {}".format(self.switches))
-            self.switchingTimes[start_time] = numpy.zeros(self.channelTotal, dtype = numpy.int8)
+            self.switchingTimes[start_time] = np.zeros(self.channelTotal, dtype = np.int8)
             self.switches += 1
             self.switchingTimes[start_time][chan] = value
 
