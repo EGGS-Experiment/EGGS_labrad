5c74dd3241fe2619202520b24349a475aa7a9fab
Clayton Ho <claytonho@g.ucla.edu>
2021-11-06 06:22:52 -0700

    wrote oscilloscope_server

--- lib/servers/trap/RF_server.py
+++ lib/servers/trap/RF_server.py
@@ -3,7 +3,7 @@
 [info]
 name = RF Server
 version = 1.0
-description = Talks to Trap Signal Generator
+description = Talks to Trap RF Generator
 
 [startup]
 cmdline = %PYTHON% %FILE%
@@ -28,21 +28,85 @@ class RFServer(GPIBManagedServer):
     name = 'RF Server'
 
     deviceWrappers = {
-        '***todo': SMY01Wrapper,
+        'ROHDE&SCHWARZ SMY01': SMY01Wrapper,
     }
 
-    # SYSTEM
-    @setting(11, returns='')
+    # GENERAL
+    @setting(111, 'Reset', returns='')
     def reset(self, c):
-        """Reset the signal generators to factory settings."""
+        """Reset the signal generator."""
         dev = self.selectedDevice(c)
         yield dev.reset()
 
-    @setting(12, returns='')
-    def clear_buffers(self, c):
-        """Clear device status buffers."""
+    @setting(112, 'Status', returns='s')
+    def status(self, c):
+        """Get status of the signal generator."""
         dev = self.selectedDevice(c)
-        yield dev.clear_buffers()
+        yield dev.status()
+
+    @setting(121, 'Toggle', onoff='b', returns='b')
+    def toggle(self, c, onoff=None):
+        """Turn the signal generator on/off."""
+        dev = self.selectedDevice(c)
+        return dev.toggle(onoff)
+
+
+    # WAVEFORM
+    @setting(211, 'Frequency', freq='v', returns='v')
+    def frequency(self, c, freq=None):
+        """Set the signal generator frequency."""
+        dev = self.selectedDevice(c)
+        return dev.freq(freq)
+
+    @setting(121, 'Amplitude', ampl='v', returns='v')
+    def amplitude(self, c, ampl=None):
+        """Set the signal generator amplitude."""
+        dev = self.selectedDevice(c)
+        return dev.ampl(ampl)
+
+
+    # MODULATION
+    @setting(311, 'AM Toggle', onoff='b', returns='b')
+    def am_toggle(self, c, onoff=None):
+        """Toggle amplitude modulation."""
+        dev = self.selectedDevice(c)
+        return dev.fm_toggle(onoff)
+
+    @setting(312, 'AM Frequency', freq='v', returns='v')
+    def am_freq(self, c, freq=None):
+        """Set amplitude modulation frequency."""
+        dev = self.selectedDevice(c)
+        return dev.am_freq(freq)
+
+    @setting(313, 'AM Depth', depth='v', returns='v')
+    def am_depth(self, c, depth=None):
+        """Set amplitude modulation depth."""
+        dev = self.selectedDevice(c)
+        return dev.am_depth(depth)
+
+    @setting(321, 'FM Toggle', onoff='b', returns='b')
+    def fm_toggle(self, c, onoff=None):
+        """Toggle frequency modulation."""
+        dev = self.selectedDevice(c)
+        return dev.fm_toggle(onoff)
+
+    @setting(322, 'FM Frequency', freq='v', returns='v')
+    def fm_freq(self, c, freq=None):
+        """Set frequency modulation frequency."""
+        dev = self.selectedDevice(c)
+        return dev.fm_freq(freq)
+
+    @setting(321, 'PM Toggle', onoff='b', returns='b')
+    def fm_toggle(self, c, onoff=None):
+        """Toggle phase modulation."""
+        dev = self.selectedDevice(c)
+        return dev.pm_toggle(onoff)
+
+    @setting(322, 'PM Frequency', freq='v', returns='v')
+    def fm_freq(self, c, freq = None):
+        """Set phase modulation frequency."""
+        dev = self.selectedDevice(c)
+        return dev.pm_freq(freq)
 
 
 if __name__ == '__main__':
