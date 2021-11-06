0cb5f9e5addff01bd5256edbb2f42718ccb773e4
Clayton Ho <claytonho@g.ucla.edu>
2021-11-06 08:01:08 -0700

    finished rohde and schwarz smy01 server

--- lib/servers/trap/RF_server.py
+++ lib/servers/trap/RF_server.py
@@ -35,78 +35,66 @@ class RFServer(GPIBManagedServer):
     @setting(111, 'Reset', returns='')
     def reset(self, c):
         """Reset the signal generator."""
-        dev = self.selectedDevice(c)
-        yield dev.reset()
+        yield self.selectedDevice(c).reset()
 
     @setting(112, 'Status', returns='s')
     def status(self, c):
         """Get status of the signal generator."""
-        dev = self.selectedDevice(c)
-        yield dev.status()
+        yield self.selectedDevice(c).status()
 
-    @setting(121, 'Toggle', onoff='b', returns='b')
+    @setting(121, 'Toggle', onoff='b', returns='')
     def toggle(self, c, onoff=None):
         """Turn the signal generator on/off."""
-        dev = self.selectedDevice(c)
-        return dev.toggle(onoff)
+        self.selectedDevice(c).toggle(onoff)
 
 
     # WAVEFORM
     @setting(211, 'Frequency', freq='v', returns='v')
     def frequency(self, c, freq=None):
-        """Set the signal generator frequency."""
-        dev = self.selectedDevice(c)
-        return dev.freq(freq)
+        """Set the signal generator frequency (in Hz)."""
+        return self.selectedDevice(c).freq(freq)
 
     @setting(121, 'Amplitude', ampl='v', returns='v')
     def amplitude(self, c, ampl=None):
-        """Set the signal generator amplitude."""
-        dev = self.selectedDevice(c)
-        return dev.ampl(ampl)
+        """Set the signal generator amplitude (in V)."""
+        return self.selectedDevice(c).ampl(ampl)
 
 
     # MODULATION
-    @setting(311, 'AM Toggle', onoff='b', returns='b')
+    @setting(311, 'Modulation Frequency', freq='v', returns='v')
+    def mod_freq(self, c, freq=None):
+        """Set modulation frequency (in Hz)."""
+        return self.selectedDevice(c).mod_freq(freq)
+
+    @setting(312, 'AM Toggle', onoff='b', returns='')
     def am_toggle(self, c, onoff=None):
         """Toggle amplitude modulation."""
-        dev = self.selectedDevice(c)
-        return dev.fm_toggle(onoff)
-
-    @setting(312, 'AM Frequency', freq='v', returns='v')
-    def am_freq(self, c, freq=None):
-        """Set amplitude modulation frequency."""
-        dev = self.selectedDevice(c)
-        return dev.am_freq(freq)
+        self.selectedDevice(c).fm_toggle(onoff)
 
     @setting(313, 'AM Depth', depth='v', returns='v')
     def am_depth(self, c, depth=None):
-        """Set amplitude modulation depth."""
-        dev = self.selectedDevice(c)
-        return dev.am_depth(depth)
+        """Set amplitude modulation depth (in %)."""
+        return self.selectedDevice(c).am_depth(depth)
 
-    @setting(321, 'FM Toggle', onoff='b', returns='b')
+    @setting(321, 'FM Toggle', onoff='b', returns='')
     def fm_toggle(self, c, onoff=None):
         """Toggle frequency modulation."""
-        dev = self.selectedDevice(c)
-        return dev.fm_toggle(onoff)
+        self.selectedDevice(c).fm_toggle(onoff)
 
-    @setting(322, 'FM Frequency', freq='v', returns='v')
-    def fm_freq(self, c, freq=None):
-        """Set frequency modulation frequency."""
-        dev = self.selectedDevice(c)
-        return dev.fm_freq(freq)
+    @setting(322, 'FM Deviation', dev='v', returns='v')
+    def fm_dev(self, c, dev=None):
+        """Set frequency modulation deviation (in Hz)."""
+        return self.selectedDevice(c).fm_dev(dev)
 
-    @setting(321, 'PM Toggle', onoff='b', returns='b')
-    def fm_toggle(self, c, onoff=None):
+    @setting(321, 'PM Toggle', onoff='b', returns='')
+    def pm_toggle(self, c, onoff=None):
         """Toggle phase modulation."""
-        dev = self.selectedDevice(c)
-        return dev.pm_toggle(onoff)
+        self.selectedDevice(c).pm_toggle(onoff)
 
-    @setting(322, 'PM Frequency', freq='v', returns='v')
-    def fm_freq(self, c, freq = None):
-        """Set phase modulation frequency."""
-        dev = self.selectedDevice(c)
-        return dev.pm_freq(freq)
+    @setting(322, 'PM Deviation', dev='v', returns='v')
+    def pm_dev(self, c, dev=None):
+        """Set phase modulation deviation (in Hz)."""
+        return self.selectedDevice(c).pm_dev(dev)
 
 
 if __name__ == '__main__':
--- lib/servers/trap/SMY01.py
+++ lib/servers/trap/SMY01.py
@@ -1,32 +1,87 @@
-from labrad import types as T, util
 from labrad.gpib import GPIBDeviceWrapper
 from twisted.internet.defer import inlineCallbacks, returnValue
-from labrad.types import Value
-from labrad.units import mV, ns
-
-import numpy as np
 
 class SMY01Wrapper(GPIBDeviceWrapper):
 
-    #system
+    # GENERAL
     @inlineCallbacks
     def reset(self):
         yield self.write('*RST')
-        # TODO wait for reset to complete
+
+    #todo: handle/clear errors
+    #todo: check that we can change modulation settings without accidentally activating modulation
 
     @inlineCallbacks
-    def clear_buffers(self):
-        yield self.write('*CLS')
+    def toggle(self, c, onoff):
+        if onoff is True:
+            yield self.write('LEV:ON')
+        elif onoff is False:
+            yield self.write('LEV:OFF')
 
-    #channel
+    # WAVEFORM
     @inlineCallbacks
-    def channel_coupling(self, channel, coupling = None):
-        chString = ':CHAN%d:COUP' %channel
-        if coupling is not None:
-            coupling = coupling.upper()
-            if coupling not in COUPLINGS:
-                raise Exception('Coupling must be either: ' + str(COUPLINGS))
-            else:
-                yield self.write(chString + ' ' + coupling)
-        resp = yield self.query(chString + '?')
+    def frequency(self, c, freq):
+        if freq:
+            yield self.write('RF ' + str(freq))
+        resp = yield float(self.query('RF?'))
         returnValue(resp)
+
+    @inlineCallbacks
+    def amplitude(self, c, ampl):
+        if ampl:
+            yield self.write('LEV '+ str(ampl) + 'V')
+        resp = yield float(self.query('LEV?'))
+        returnValue(resp)
+
+
+    # MODULATION
+    @inlineCallbacks
+    def mod_freq(self, c, freq):
+        if freq:
+            yield self.write('AF ' + str(freq))
+        resp = yield float(self.query('AF?'))
+        returnValue(resp)
+
+    @inlineCallbacks
+    def am_toggle(self, c, onoff):
+        if onoff is True:
+            yield self.write('AM:I')
+        elif onoff is False:
+            yield self.write('AM:OFF')
+
+    @inlineCallbacks
+    def am_depth(self, c, depth):
+        if depth:
+            yield self.write('AM ' + str(depth))
+        resp = yield float(self.query('AM?'))
+        returnValue(resp)
+
+    @inlineCallbacks
+    def fm_toggle(self, c, onoff):
+        if onoff is True:
+            yield self.write('FM:I')
+        elif onoff is False:
+            yield self.write('FM:OFF')
+
+    @inlineCallbacks
+    def fm_dev(self, c, dev):
+        if dev:
+            yield self.write('FM ' + str(dev))
+        resp = yield float(self.query('FM?'))
+        returnValue(resp)
+
+    @inlineCallbacks
+    def pm_toggle(self, c, onoff):
+        if onoff is True:
+            yield self.write(chstring + 'PHM:I')
+        elif onoff is False:
+            yield self.write(chstring + 'PHM:OFF')
+
+    @inlineCallbacks
+    def pm_dev(self, c, dev):
+        if dev:
+            yield self.write('PHM ' + str(dev))
+        resp = yield float(self.query('PHM?'))
+        returnValue(resp)
+
+
