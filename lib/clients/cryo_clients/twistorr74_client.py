e60930114f06e65f65988575990f47a88b1b9119
Clayton Ho <claytonho@g.ucla.edu>
2021-11-10 21:44:03 -0800

    updating cryo clients

---
+++ lib/clients/cryo_clients/twistorr74_client.py
@@ -0,0 +1,115 @@
+import os, socket
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout, QGridLayout
+from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.internet.task import LoopingCall
+from EGGS_labrad.lib.clients.pump_client.pump_gui import pump_gui
+
+from EGGS_labrad.lib.clients.connection import connection
+
+class twistorr74_client(QWidget):
+    #todo: make connections inheritable
+    name = 'Twistorr74 Client'
+    LABRADPASSWORD = os.environ['LABRADPASSWORD']
+
+    def __init__(self, reactor, parent=None):
+        super(pump_client, self).__init__()
+        self.reactor = reactor
+        self.connect()
+        self.initializeGUI()
+
+        #create and start loop to poll server for temperature
+        # self.temp_loop = LoopingCall(self.poll)
+        # self.start_polling()
+
+    #Setup functions
+    @inlineCallbacks
+    def connect(self):
+        """
+        Creates an asynchronous connection to pump servers
+        and relevant labrad servers
+        """
+        #from labrad.wrappers import connectAsync
+        #self.cxn = yield connectAsync('localhost', name = 'Pump Client', password = self.LABRADPASSWORD)
+        self.cxn = connection(name = self.name)
+        yield self.cxn.connect()
+        self.context = yield self.cxn.context()
+        self.reg = yield self.cxn.get_server('Registry')
+        self.dv = yield self.cxn.get_server('Data Vault')
+        #self.turbo = yield self.cxn.get_server('twistorr_74_server')
+
+        # get polling time
+        yield self.reg.cd(['Clients', self.name])
+        self.poll_time = yield float(self.reg.get('poll_time'))
+
+        # set recording stuff
+        self.c_press = self.cxn.context()
+        self.recording = False
+
+    #@inlineCallbacks
+    def initializeGUI(self):
+        #initialize main GUI
+        layout = QGridLayout()
+        self.gui = pump_gui(parent = self)
+        layout.addWidget(self.gui)
+        self.setLayout(layout)
+        self.setWindowTitle(self.name)
+
+        #connect signals to slots
+        self.gui.twistorr_lockswitch.toggled.connect(lambda: self.lock_twistorr())
+        self.gui.twistorr_power.toggled.connect(lambda: self.toggle_twistorr())
+        self.gui.twistorr_record.toggled.connect(lambda: self.record_pressure())
+
+        #start up data
+
+    #Slot functions
+    @inlineCallbacks
+    def record_pressure(self):
+        """
+        Creates a new dataset to record pressure and tells polling loop
+        to add data to data vault
+        """
+        self.recording = self.gui.press_record.isChecked()
+        if self.recording == True:
+            yield self.dv.cd(['', year, month, trunk1, trunk2], True, context = self.c_press)
+            yield self.dv.new('Twistorr 74 Pump Controller', [('Elapsed time', 't')], [('Pump Pressure', 'Pressure', 'mbar')], context=self.c_press)
+
+    @inlineCallbacks
+    def toggle_twistorr(self):
+        """
+        Sets pump power on or off
+        """
+        power_status = self.gui.twistorr_power.isChecked()
+        yield self.pump.toggle(power_status)
+
+    def lock_twistorr(self):
+        """
+        Locks power status of pump
+        """
+        lock_status = self.gui.twistorr_lockswitch.isChecked()
+        self.gui.twistorr_power.setEnabled(lock_status)
+
+    #Polling functions
+    def start_polling(self):
+        self.press_loop.start(self.poll_time)
+
+    def stop_polling(self):
+        self.press_loop.stop()
+
+    def poll(self):
+        pressure = yield self.pump.read_pressure()
+        self.gui.press_display.setText(str(pressure))
+        if self.recording == True:
+            yield self.dv.add(elapsedtime, pressure, context=self.c_press)
+
+    def closeEvent(self, event):
+        self.reactor.stop()
+
+if __name__ == "__main__":
+    a = QApplication([])
+    import qt5reactor
+    qt5reactor.install()
+    from twisted.internet import reactor
+    pump_interface = pump_client(reactor)
+    pump_interface.show()
+    reactor.run()
---
+++ lib/clients/cryo_clients/twistorr74_gui.py
@@ -0,0 +1,72 @@
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QFont
+import sys
+
+from EGGS_labrad.lib.clients.Widgets import TextChangingButton as _TextChangingButton
+
+class TextChangingButton(_TextChangingButton):
+    def __init__(self, button_text=None, parent=None):
+        super(TextChangingButton, self).__init__(button_text, parent)
+        self.setMaximumHeight(30)
+
+class pump_gui(QtWidgets.QFrame):
+    def __init__(self, parent=None):
+        window = QtWidgets.QWidget.__init__(self, parent)
+        self.setFrameStyle(0x0001 | 0x0030)
+        self.makeWidgets()
+        self.makeLayout()
+        self.setWindowTitle("Pump Clients")
+
+    def makeWidgets(self):
+        shell_font = 'MS Shell Dlg 2'
+
+        #twistorr 74
+        self.twistorr_label = QtWidgets.QLabel('Twistorr 74 Pump')
+        self.twistorr_label.setFont(QFont(shell_font, pointSize= 20))
+        self.twistorr_label.setAlignment(QtCore.Qt.AlignCenter)
+            #readout
+        self.twistorr_display_label = QtWidgets.QLabel('Pressure (mbar)')
+        self.twistorr_display = QtWidgets.QLabel('Pressure')
+        self.twistorr_display.setFont(QFont(shell_font, pointSize=25))
+        self.twistorr_display.setAlignment(QtCore.Qt.AlignCenter)
+        self.twistorr_display.setStyleSheet('color: blue')
+            #record button
+        self.twistorr_record = TextChangingButton(('Stop Recording', 'Start Recording'))
+            #power
+        self.twistorr_lockswitch = TextChangingButton(('Lock', 'Unlock'))
+        self.twistorr_power = TextChangingButton(('On', 'Off'))
+
+        #niops03
+        self.niops_label = QtWidgets.QLabel('NIOPS 03 Pump')
+        self.niops_label.setFont(QFont(shell_font, pointSize= 20))
+        self.niops_label.setAlignment(QtCore.Qt.AlignCenter)
+            #power
+        self.niops_lockswitch = TextChangingButton(('Lock', 'Unlock'))
+        self.niops_power = TextChangingButton(('On', 'Off'))
+
+    def makeLayout(self):
+        layout = QtWidgets.QGridLayout()
+        shell_font = 'MS Shell Dlg 2'
+
+        pump1_col = 1
+        pump2_col = 9
+
+        layout.addWidget(self.twistorr_label, 1, pump1_col)
+        layout.addWidget(self.twistorr_display_label, 2, pump1_col)
+        layout.addWidget(self.twistorr_display, 3, pump1_col, 3, 5)
+        layout.addWidget(self.twistorr_power, 7, pump1_col, 1, 5)
+        layout.addWidget(self.twistorr_lockswitch, 8, pump1_col, 1, 5)
+        layout.addWidget(self.twistorr_record, 9, pump1_col, 1, 5)
+
+        layout.minimumSize()
+        self.setLayout(layout)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    icon = pump_gui()
+    icon.show()
+    app.exec_()
+
+
+
+
