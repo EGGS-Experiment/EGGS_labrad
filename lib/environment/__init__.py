1d35090f39bcc85b58bd152022f41c3f3dc34951
Clayton Ho <claytonho@g.ucla.edu>
2021-11-10 16:36:02 -0800

    minor cleanup

--- lib/environment/__init__.py
+++ lib/environment/__init__.py
@@ -1,3 +1,9 @@
+#stores all classes for regular use with labrad
+
+#experiments
+
+#servers
+
 #pulse sequence objects
 from .pulse_sequence import pulse_sequence
 from .plot_sequence import SequencePlotter
