186d438c421c663275e5453f808a8d102ea87ee5
Clayton Ho <claytonho@g.ucla.edu>
2021-11-21 02:34:57 -0800

    bugfixes to sls_cxn.bat

--- bin/sls_cxn.bat
+++ bin/sls_cxn.bat
@@ -1,5 +1,5 @@
 @echo off && conda activate labart && python -ix "%~f0" %* & goto :eof
 import labrad
 cxn = labrad.connect()
-tt=cxn.sls_server
-tt.select_device('hengfachuen','COM6')
+sls=cxn.sls_server
+sls.select_device('hengfachuen','COM6')
