351ac02818288c4a42a79c7aec2ba0515d0f2b83
Clayton Ho <claytonho@g.ucla.edu>
2021-11-27 09:38:42 -0800

    artiq batch file now starts master in a way that allows other clients to access it

--- bin/start_artiq.bat
+++ bin/start_artiq.bat
@@ -2,7 +2,7 @@
 
 ::Starts all components necessary for running ARTIQ
 
-start "ARTIQ Master" /min cmd "/k activate labart2 && artiq_master -g -r %ARTIQ_ROOT%/repository --device-db %ARTIQ_ROOT%/device_db.py"
+start "ARTIQ Master" /min cmd "/k activate labart2 && artiq_master -g -r %ARTIQ_ROOT%/repository --device-db %ARTIQ_ROOT%/device_db.py --bind=192.168.1.28"
 start "ARTIQ Dashboard" /min cmd "/k activate labart2 && artiq_dashboard"
 start "ARTIQ Controller Manager" /min cmd "/k activate labart2 && artiq_ctlmgr"
 
