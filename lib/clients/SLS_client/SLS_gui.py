45f5073b9ecfda223f49ff421ee32f7c280ea13d
Clayton Ho <claytonho@g.ucla.edu>
2021-12-03 04:06:02 -0800

    renamed sls files

--- lib/clients/SLS_client/SLS_gui.py
+++ lib/clients/SLS_client/SLS_gui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'sls_gui.ui'
+# Form implementation generated from reading ui file 'SLS_gui.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
