ac0518a28f2b683487b93b2b3007d3e3625598ca
Clayton Ho <claytonho@g.ucla.edu>
2021-12-07 01:51:14 -0800

    created fma1700a gui

--- lib/clients/cryovac_clients/fma1700a_gui.py
+++ lib/clients/cryovac_clients/fma1700a_gui.py
@@ -1,5 +1,6 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
-from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QFrame, QWidget, QLabel, QGridLayout
 
 from EGGS_labrad.lib.clients.Widgets import TextChangingButton as _TextChangingButton
 
@@ -7,57 +8,56 @@ from EGGS_labrad.lib.clients.Widgets import TextChangingButton as _TextChangingB
 class TextChangingButton(_TextChangingButton):
     def __init__(self, button_text=None, parent=None):
         super(TextChangingButton, self).__init__(button_text, parent)
-        self.setMaximumHeight(30)
+        self.setMaximumHeight(25)
 
 
-class twistorr74_gui(QtWidgets.QFrame):
+class fma1700a_gui(QFrame):
     def __init__(self, parent=None):
-        window = QtWidgets.QWidget.__init__(self, parent)
+        window = QWidget.__init__(self, parent)
         self.setFrameStyle(0x0001 | 0x0030)
         self.makeWidgets()
         self.makeLayout()
-        self.setWindowTitle("Twistorr74 Client")
+        self.setWindowTitle("FMA1700A Client")
 
     def makeWidgets(self):
         shell_font = 'MS Shell Dlg 2'
-        self.setFixedSize(225, 225)
-        #twistorr 74
-        self.twistorr_label = QtWidgets.QLabel('Twistorr 74 Pump')
-        self.twistorr_label.setFont(QFont(shell_font, pointSize= 18))
-        self.twistorr_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.setFixedSize(200, 200)
+        # title
+        self.all_label = QLabel('FMA1700A')
+        self.all_label.setFont(QFont(shell_font, pointSize=18))
+        self.all_label.setAlignment(Qt.AlignCenter)
             #readout
-        self.twistorr_display_label = QtWidgets.QLabel('Pressure (mbar)')
-        self.twistorr_display = QtWidgets.QLabel('Pressure')
-        self.twistorr_display.setFont(QFont(shell_font, pointSize=20))
-        self.twistorr_display.setAlignment(QtCore.Qt.AlignCenter)
-        self.twistorr_display.setStyleSheet('color: blue')
+        self.flow_display_label = QLabel('Flow (L/min)')
+        self.flow_display = QLabel('Flow')
+        self.flow_display.setFont(QFont(shell_font, pointSize=20))
+        self.flow_display.setAlignment(Qt.AlignCenter)
+        self.flow_display.setStyleSheet('color: blue')
             #record button
-        self.twistorr_record = TextChangingButton(('Stop Recording', 'Start Recording'))
+        self.record_button = TextChangingButton(('Stop Recording', 'Start Recording'))
             #power
-        self.twistorr_lockswitch = TextChangingButton(('Unlocked', 'Locked'))
-        self.twistorr_lockswitch.setChecked(True)
-        self.twistorr_power = TextChangingButton(('On', 'Off'))
+        self.power_button = TextChangingButton(('On', 'Off'))
+        self.lockswitch = TextChangingButton(('Unlocked', 'Locked'))
+        self.lockswitch.setChecked(True)
 
     def makeLayout(self):
-        layout = QtWidgets.QGridLayout()
-        shell_font = 'MS Shell Dlg 2'
+        layout = QGridLayout()
 
-        pump1_col = 0
-        #pump2_col = 9
+        col1 = 0
+        # col2 = 1
 
-        layout.addWidget(self.twistorr_label, 0, pump1_col)
-        layout.addWidget(self.twistorr_display_label, 1, pump1_col)
-        layout.addWidget(self.twistorr_display, 2, pump1_col)
-        layout.addWidget(self.twistorr_power, 3, pump1_col)
-        layout.addWidget(self.twistorr_lockswitch, 4, pump1_col)
-        layout.addWidget(self.twistorr_record, 5, pump1_col)
+        layout.addWidget(self.all_label, 0, col1)
+        layout.addWidget(self.flow_display_label, 1, col1)
+        layout.addWidget(self.flow_display, 2, col1)
+        layout.addWidget(self.power_button, 3, col1)
+        layout.addWidget(self.lockswitch, 4, col1)
+        layout.addWidget(self.record_button, 5, col1)
 
         #layout.minimumSize()
         self.setLayout(layout)
 
 if __name__ == "__main__":
     from EGGS_labrad.lib.clients import runGUI
-    runGUI(twistorr74_gui)
+    runGUI(fma1700a_gui)
 
 
 
