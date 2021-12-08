49c305de6ca5e424eb7a8e18f483ee047e383759
Clayton Ho <claytonho@g.ucla.edu>
2021-12-07 23:57:18 -0800

    further fixes to rga

--- lib/servers/cryovac/RGA_server.py
+++ lib/servers/cryovac/RGA_server.py
@@ -14,6 +14,7 @@ message = 987654321
 timeout = 5
 ### END NODE INFO
 """
+import numpy as np
 
 from labrad.units import WithUnit
 from labrad.server import Signal, setting
@@ -232,6 +233,8 @@ class RGA_Server(SerialDeviceServer):
         if (num_scans < 0) or (num_scans > 255):
             raise Exception('Invalid Input.')
 
+        # get initial and final masses
+
         # create scan message
         msg = None
         bytes_to_read = 0
@@ -256,11 +259,12 @@ class RGA_Server(SerialDeviceServer):
 
         # process scan
         current_arr = [int.from_bytes(resp[i:i+4], 'big') for i in range(0, bytes_to_read, 4)]
+        # create axis
         returnValue(current_arr)
 
 
     # SINGLE MASS MEASUREMENT
-    @setting(511, 'SMM Start', mass='i', returns='v')
+    @setting(511, 'SMM Start', mass='i', returns='w')
     def singleMassMeasurement(self, c, mass):
         """
         Start a single mass measurement.
@@ -270,39 +274,43 @@ class RGA_Server(SerialDeviceServer):
 
         # start a single mass measurement
         msg = 'MR' + str(mass) + _SRS_EOL
-
         yield self.comm_lock.acquire()
         yield self.ser.write(msg)
-        resp = yield self.ser.read_line(_SRS_EOL)
+        resp = yield self.ser.read(4)
         yield self.comm_lock.release()
 
         # set the rods back to zero
-        yield self._query('MR', 0, True, False)
+        yield self.ser.write('MR0\r')
+
+        # process result
+        current = int.from_bytes(resp, 'big')
 
         # return the result
-        returnValue(float(resp))
+        returnValue(current)
 
 
     # TOTAL PRESSURE MEASUREMENT
-    @setting(611, 'TPM Start', returns='v')
+    @setting(611, 'TPM Start', returns='w')
     def totalPressureMeasurement(self, c):
         """
         Start a total pressure measurement.
         """
         # set the electron multiplier voltage to zero
         # which automatically enables total pressure measurement
-        yield self._query('HV', 0, True, False)
+        yield self._setter('HV', 0)
 
         # start a total pressure measurement
         msg = 'TP?' + _SRS_EOL
-
         yield self.comm_lock.acquire()
         yield self.ser.write(msg)
-        resp = yield self.ser.read_line(_SRS_EOL)
+        resp = yield self.ser.read(4)
         yield self.comm_lock.release()
 
+        # process result
+        pressure = int.from_bytes(resp, 'big')
+
         # return the result
-        returnValue(float(resp))
+        returnValue(pressure)
 
     # HELPER
     @inlineCallbacks
