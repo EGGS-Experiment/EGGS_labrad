cf68ec762f3705166650683aee5d609e98d3b354
Clayton Ho <claytonho@g.ucla.edu>
2022-02-01 00:31:51 -0800

    started and gave up on the HP6256B client-end since we're probably getting a different psu

---
+++ EGGS_labrad/clients/loading_clients/HP6256B_client.py
@@ -0,0 +1,56 @@
+from twisted.internet.defer import inlineCallbacks
+
+from EGGS_labrad.clients import GUIClient
+from EGGS_labrad.clients.loading_clients.HP6256B_gui import HP6256B_gui
+
+
+class HP6256B_client(GUIClient):
+    """
+    Client for HP6256B control via ARTIQ.
+    """
+
+    name = "HP6256B Client"
+    HVID = 374861
+    servers = {'hp': 'HP6256B Server'}
+
+    def getgui(self):
+        if self.gui is None:
+            self.gui = HP6256B_gui()
+        return self.gui
+
+    @inlineCallbacks
+    def initClient(self):
+        # connect to signals
+        yield self.hp.signal__hv_update(self.HVID)
+        yield self.hp.addListener(listener=self.updateHV, source=None, ID=self.HVID)
+
+    @inlineCallbacks
+    def initData(self):
+        self.gui.dac.setValue(voltage_list[i])
+        self.gui.toggle.setChecked(toggle_list[i])
+
+    def initializeGUI(self):
+        # connect global signals
+        self.gui.device_global_onswitch.clicked.connect(lambda: self.hp.toggle(-1, 1))
+        self.gui.device_global_offswitch.clicked.connect(lambda: self.hp.toggle(-1, 0))
+        self.gui.device_global_clear.clicked.connect(lambda: self.hp.clear())
+        # connect each channel
+        for channel in self.gui.amo8_channels:
+            channel.dac.valueChanged.connect(lambda value: self.hp.voltage(channel.number, value))
+            channel.ramp_start.clicked.connect(lambda voltage=channel.ramp_target.value(), rate=channel.ramp_rate.value():
+                                               self.hp.ramp(channel.number, voltage, rate))
+            channel.toggle.clicked.connect(lambda status=channel.lockswitch.isChecked():
+                                           self.hp.toggle(channel.number, status))
+            channel.resetswitch.clicked.connect(lambda: self.reset(channel.number))
+
+
+    # SLOTS
+    @inlineCallbacks
+    def reset(self, channel_num):
+        yield self.hp.voltage(channel_num, 0)
+        yield self.hp.toggle(channel_num, 0)
+
+
+if __name__ == "__main__":
+    from EGGS_labrad.clients import runClient
+    runClient(HP6256B_client)
---
+++ EGGS_labrad/clients/loading_clients/HP6256B_gui.py
@@ -0,0 +1,85 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QWidget, QDoubleSpinBox, QLabel, QGridLayout, QFrame, QPushButton
+
+from twisted.internet.defer import inlineCallbacks
+from EGGS_labrad.clients.Widgets import TextChangingButton, Lockswitch
+
+
+class HP6256B_gui(QFrame):
+    """
+    GUI for the HP6256B power supply controlled by the ARTIQ DAC.
+    """
+
+    def __init__(self, name=None, num=None, parent=None):
+        QWidget.__init__(self, parent)
+        self.name = name
+        self.number = num
+        self.setFrameStyle(0x0001 | 0x0030)
+        self.setFixedSize(275, 225)
+        self.makeLayout(name)
+
+    def makeLayout(self, title):
+        layout = QGridLayout()
+        # title
+        self.title = QLabel(title)
+        self.title.setFont(QFont('MS Shell Dlg 2', pointSize=16))
+        self.title.setAlignment(Qt.AlignCenter)
+        # dac
+        self.dac_label = QLabel('Output Voltage (V)')
+        self.dac = QDoubleSpinBox()
+        self.dac.setFont(QFont('MS Shell Dlg 2', pointSize=16))
+        self.dac.setDecimals(2)
+        self.dac.setSingleStep(0.01)
+        self.dac.setRange(0, 850)
+        self.dac.setKeyboardTracking(False)
+        # ramp
+        self.ramp_start = QPushButton('Ramp')
+        self.ramp_start.setFont(QFont('MS Shell Dlg 2', pointSize=12))
+        self.ramp_target_label = QLabel('End Voltage (V)')
+        self.ramp_target = QDoubleSpinBox()
+        self.ramp_target.setFont(QFont('MS Shell Dlg 2', pointSize=16))
+        self.ramp_target.setDecimals(2)
+        self.ramp_target.setSingleStep(0.01)
+        self.ramp_target.setRange(0, 850)
+        self.ramp_target.setKeyboardTracking(False)
+        self.ramp_rate_label = QLabel('Ramp Rate (V/s)')
+        self.ramp_rate = QDoubleSpinBox()
+        self.ramp_rate.setFont(QFont('MS Shell Dlg 2', pointSize=16))
+        self.ramp_rate.setDecimals(2)
+        self.ramp_rate.setSingleStep(0.01)
+        self.ramp_rate.setRange(0, 10)
+        self.ramp_rate.setKeyboardTracking(False)
+        # buttons
+        self.toggleswitch = TextChangingButton(("On", "Off"))
+        self.resetswitch = QPushButton('Reset')
+        self.resetswitch.setFont(QFont('MS Shell Dlg 2', pointSize=10))
+        self.lockswitch = Lockswitch()
+        self.lockswitch.setChecked(True)
+
+        # add widgets to layout
+        layout.addWidget(self.title, 0, 0, 1, 3)
+        layout.addWidget(self.dac_label, 1, 0, 1, 2)
+        layout.addWidget(self.dac, 2, 0, 1, 3)
+        layout.addWidget(self.ramp_target_label, 3, 1)
+        layout.addWidget(self.ramp_rate_label, 3, 2)
+        layout.addWidget(self.ramp_start, 4, 0, 1, 1)
+        layout.addWidget(self.ramp_target, 4, 1, 1, 1)
+        layout.addWidget(self.ramp_rate, 4, 2, 1, 1)
+        layout.addWidget(self.toggleswitch, 5, 0)
+        layout.addWidget(self.lockswitch, 5, 1)
+        layout.addWidget(self.resetswitch, 5, 2)
+        self.setLayout(layout)
+        # connect signal to slot
+        self.lockswitch.toggled.connect(lambda status=self.lockswitch.isChecked(): self.lock(status))
+
+    @inlineCallbacks
+    def lock(self, status):
+        yield self.resetswitch.setEnabled(status)
+        yield self.dac.setEnabled(status)
+
+
+
+if __name__ == "__main__":
+    from EGGS_labrad.clients import runGUI
+    runGUI(HP6256B_gui, name='HP6256B Client')
