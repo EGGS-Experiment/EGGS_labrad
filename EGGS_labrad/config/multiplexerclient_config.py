f374a91f6352ae94013c801daf2981ca222ed692
Clayton Ho <claytonho@g.ucla.edu>
2022-03-15 05:31:32 -0700

    minor edits to wavemeter config

--- EGGS_labrad/config/multiplexerclient_config.py
+++ EGGS_labrad/config/multiplexerclient_config.py
@@ -3,17 +3,17 @@ class multiplexer_config(object):
     Multiplexer client configuration file.
     Attributes
     ----------
-    info: dict
+    channels: dict
     {
-        channel_name: (signal_port, frequency, display_location, stretched, display_pid, dac_port, dac_rails)
+        channel_name: (signal_port, frequency, display_location, stretched, display_pid, dac_port, dac_rails, displayPattern)
     }
     """
 
-    info = {
-        '397nm': (5, '461.251000', (0, 1), False, False, 4, [-4, 4], True),
+    channels = {
+        '397nm': (5, '755.22244', (0, 1), False, False, 4, [-4, 4], True),
         '423nm': (6, '709.07767', (0, 2), True, False, 5, [-4, 4], True),
-        # '854nm': (13, '607.616000', (0, 3), True, False, 12, [-4, 4], True)
-        # '866nm': (14, '607.616000', (0, 4), True, False, 13, [-4, 4], True),
+        # '854nm': (13, '350.86265', (0, 3), True, False, 12, [-4, 4], True)
+        # '866nm': (14, '346.00002', (0, 4), True, False, 13, [-4, 4], True),
     }
 
     ip = '10.97.111.8'
