d2ed49aaa6fd5ad8290108c560b84f035918b229
Clayton Ho <claytonho@g.ucla.edu>
2022-03-19 03:50:42 -0700

    minor edits

--- EGGS_labrad/config/dc_config.py
+++ EGGS_labrad/config/dc_config.py
@@ -16,8 +16,10 @@ class dc_config(object):
     row_length = 2
 
     channeldict = {
-        'Endcap 1': {'num': 0, 'row': 1, 'col': 0},
-        'Endcap 2': {'num': 1, 'row': 1, 'col': 1},
-        'Shim 1': {'num': 2, 'row': 2, 'col': 0},
-        'Shim 2': {'num': 3, 'row': 2, 'col': 1},
+        'Endcap 1':     {'num': 0, 'row': 1, 'col': 0},
+        'Endcap 2':     {'num': 1, 'row': 1, 'col': 1},
+        'Shim 1':       {'num': 2, 'row': 2, 'col': 0},
+        'Shim 2':       {'num': 3, 'row': 2, 'col': 1},
+        'Shim 3':       {'num': 4, 'row': 2, 'col': 3},
+        'Shim 4':       {'num': 5, 'row': 2, 'col': 4},
     }
