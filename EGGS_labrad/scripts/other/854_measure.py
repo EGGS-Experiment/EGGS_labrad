eddbaa14b6aeef62df10904eecf50001a429a5ad
Clayton Ho <claytonho@g.ucla.edu>
2022-03-23 22:04:37 -0700

    added laser measuring script

---
+++ EGGS_labrad/scripts/other/854_measure.py
@@ -0,0 +1,30 @@
+import labrad
+
+from numpy import arange
+from time import time, sleep
+start_time = time()
+
+# connect to labrad
+cxn_wm = labrad.connect('10.97.111.8', password='lab')
+wm = cxn_wm.multiplexerserver
+cr_wm = cxn_wm.context()
+
+cxn_eggs = labrad.connect('192.168.1.28', password='lab')
+dv = cxn_eggs.data_vault
+cr_dv = cxn_eggs.context()
+
+
+# create dataset
+dv.cd(['', '397_1'], True, context=cr_dv)
+dv.new('397nm Power', [('Elapsed time', 's')], [('397', 'Power', 'arb')], context=cr_dv)
+
+# start recording
+starttime = time()
+while True:
+    # take rga data
+    freq = wm.get_amplitude(5)
+    # print(res)
+    elapsedtime = time() - starttime
+    dv.add(elapsedtime, freq, context=cr_dv)
+    sleep(5)
+
