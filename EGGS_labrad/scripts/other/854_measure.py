5e332fbfc95c685c558585e4e44abf62b5597fcf
Clayton Ho <claytonho@g.ucla.edu>
2022-03-25 05:35:38 -0700

    updated laser measure script

--- EGGS_labrad/scripts/other/854_measure.py
+++ EGGS_labrad/scripts/other/854_measure.py
@@ -1,29 +1,29 @@
 import labrad
-
-from numpy import arange
 from time import time, sleep
-start_time = time()
+from datetime import datetime
 
-# connect to labrad
+# connect to eggs labrad
 cxn_wm = labrad.connect('10.97.111.8', password='lab')
 wm = cxn_wm.multiplexerserver
 cr_wm = cxn_wm.context()
-
+# connect to wavemeter labrad
 cxn_eggs = labrad.connect('localhost', password='lab')
 dv = cxn_eggs.data_vault
 cr_dv = cxn_eggs.context()
 
-
 # create dataset
-dv.cd(['', '397_2'], True, context=cr_dv)
+date = datetime.now()
+year = str(date.year)
+month = '{:02d}'.format(date.month)
+trunk1 = '{0:s}_{1:s}_{2:02d}'.format(year, month, date.day)
+trunk2 = '{0:s}_{1:02d}:{2:02d}'.format('397 Measure', date.hour, date.minute)
+dv.cd(['', year, month, trunk1, trunk2], True, context=cr_dv)
 dv.new('397nm Power', [('Elapsed time', 's')], [('397', 'Power', 'arb')], context=cr_dv)
 
 # start recording
 starttime = time()
 while True:
-    # take rga data
     freq = wm.get_amplitude(5)
-    # print(res)
     elapsedtime = time() - starttime
     dv.add(elapsedtime, freq, context=cr_dv)
     sleep(5)
