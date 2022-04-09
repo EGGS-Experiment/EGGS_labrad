570519bcdc052abe31fe841b01a78e5abc9c3c17
Clayton Ho <claytonho@g.ucla.edu>
2022-04-09 03:56:39 -0700

    minor bugfix in labrad_master

--- bin/labrad_master.bat
+++ bin/labrad_master.bat
@@ -23,7 +23,7 @@ START "Labrad SysLog" /min CMD "/k activate labart && python %EGGS_LABRAD_ROOT%\
 REM: Core Servers
 START "Labrad Manager" /min %HOME%\Code\scalabrad-0.8.3\bin\labrad.bat --tls-required false
 START "Labrad Web GUI" /min %HOME%\Code\scalabrad-web-server-2.0.6\bin\labrad-web.bat
-START "Labrad Node" /min CMD "/k activate labart && python %HOME%\Code\pylabrad\labrad\node\__init__.py -s -x %LABRADHOST%:%EGGS_LABRAD_SYSLOG_PORT"
+START "Labrad Node" /min CMD "/k activate labart && python %HOME%\Code\pylabrad\labrad\node\__init__.py -s -x %LABRADHOST%:%EGGS_LABRAD_SYSLOG_PORT%"
 START "" "%ProgramFiles(x86)%\chrome-win\chrome.exe" http://localhost:7667
 
 REM: Don't open any servers if raw flag is active
