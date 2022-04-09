4b03e916541d431bd4ba572ece40545357d443f4
Clayton Ho <claytonho@g.ucla.edu>
2022-04-09 03:55:16 -0700

    created a working system for network logging from all nodes

---
+++ bin/labrad_syslog.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+## Tiny Syslog Server in Python.
+##
+## This is a tiny syslog server that is able to receive UDP based syslog
+## entries on a specified port and save them to a file.
+## That's it... it does nothing else...
+## There are a few configuration parameters.
+from datetime import datetime
+LOG_FILE = 'C:\\Users\\EGGS1\\Documents\\.labrad\\logfiles\\youlogfile.log'
+HOST, PORT = "0.0.0.0", 514
+
+#
+# NO USER SERVICEABLE PARTS BELOW HERE...
+#
+
+import logging
+import socketserver
+
+logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='', filename=LOG_FILE, filemode='a')
+
+class SyslogUDPHandler(socketserver.BaseRequestHandler):
+
+	def handle(self):
+		data = bytes.decode(self.request[0].strip())
+		socket = self.request[1]
+		print( "%s : " % self.client_address[0], str(data))
+		logging.info(str(data))
+
+if __name__ == "__main__":
+	try:
+		server = socketserver.UDPServer((HOST,PORT), SyslogUDPHandler)
+		server.serve_forever(poll_interval=0.5)
+	except (IOError, SystemExit):
+		raise
+	except KeyboardInterrupt:
+		print ("Crtl+C Pressed. Shutting down.")
