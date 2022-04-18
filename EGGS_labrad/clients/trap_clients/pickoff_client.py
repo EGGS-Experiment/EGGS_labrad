2fdd1af105dfe8d60dfe6d6355dcba7ea5a8b05f
Clayton Ho <claytonho@g.ucla.edu>
2022-04-18 02:32:36 -0700

    progress on pickoff client

--- EGGS_labrad/clients/trap_clients/pickoff_client.py
+++ EGGS_labrad/clients/trap_clients/pickoff_client.py
@@ -3,32 +3,29 @@ from datetime import datetime
 from twisted.internet.defer import inlineCallbacks
 
 from EGGS_labrad.clients import GUIClient
-from EGGS_labrad.clients.cryovac_clients.fma1700a_gui import fma1700a_gui
+from EGGS_labrad.clients.trap_clients.pickoff_gui import pickoff_gui
+
+VOLTAGEID = 788135
 
 
-class fma1700a_client(GUIClient):
+class pickoff_client(GUIClient):
 
-    name = 'FMA1700A Client'
-    FLOWID = 877920
-    servers = {'fma': 'FMA1700A Server'}
+    name = 'Pickoff Client'
+    servers = {'os': 'Oscilloscope Server'}
 
     def getgui(self):
         if self.gui is None:
-            self.gui = fma1700a_gui()
+            self.gui = pickoff_gui()
         return self.gui
 
     @inlineCallbacks
     def initClient(self):
+        # connect to oscilloscope
+        yield self.os.select_device()
         # set recording stuff
         self.c_record = self.cxn.context()
         self.recording = False
-        # connect to signals
-        yield self.fma.signal__flow_update(self.FLOWID)
-        yield self.fma.addListener(listener=self.updateFlow, source=None, ID=self.FLOWID)
-        # start device polling if not already started
-        poll_params = yield self.fma.polling()
-        if not poll_params[0]:
-            yield self.fma.polling(True, 5.0)
+        # create loopingcall
 
     def initGUI(self):
         self.gui.record_button.toggled.connect(lambda status: self.record_flow(status))
@@ -48,24 +45,23 @@ class fma1700a_client(GUIClient):
             date = datetime.now()
             year = str(date.year)
             month = '{:02d}'.format(date.month)
-
             trunk1 = '{0:s}_{1:s}_{2:02d}'.format(year, month, date.day)
             trunk2 = '{0:s}_{1:02d}:{2:02d}'.format(self.name, date.hour, date.minute)
             yield self.dv.cd(['', year, month, trunk1, trunk2], True, context=self.c_record)
-            yield self.dv.new('FMA1700A Flowmeter', [('Elapsed time', 't')],
-                                       [('Flowmeter', 'Flow rate', 'L/min')], context=self.c_record)
+            yield self.dv.new('Helical Resonator Pickoff', [('Elapsed time', 't')],
+                              [('Pickoff', 'Peak-Peak Voltage', 'V')], context=self.c_record)
 
     @inlineCallbacks
-    def updateFlow(self, c, flow):
+    def updateVoltage(self, voltage):
         """
         Updates GUI when values are received from server.
         """
-        self.gui.flow_display.setText(str(flow))
+        self.gui.voltage_display.setText(str(voltage))
         if self.recording:
             elapsedtime = time.time() - self.starttime
-            yield self.dv.add(elapsedtime, flow, context=self.c_record)
+            yield self.dv.add(elapsedtime, voltage, context=self.c_record)
 
 
 if __name__ == "__main__":
     from EGGS_labrad.clients import runClient
-    runClient(fma1700a_client)
+    runClient(pickoff_client)
--- EGGS_labrad/clients/trap_clients/pickoff_gui.py
+++ EGGS_labrad/clients/trap_clients/pickoff_gui.py
@@ -5,28 +5,28 @@ from PyQt5.QtWidgets import QFrame, QWidget, QLabel, QGridLayout
 from EGGS_labrad.clients.Widgets import TextChangingButton
 
 
-class fma1700a_gui(QFrame):
+class pickoff_gui(QFrame):
 
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.setFrameStyle(0x0001 | 0x0030)
         self.makeWidgets()
         self.makeLayout()
-        self.setWindowTitle("FMA1700A Client")
+        self.setWindowTitle("Pickoff Client")
 
     def makeWidgets(self):
         shell_font = 'MS Shell Dlg 2'
         self.setFixedSize(190, 150)
         # title
-        self.all_label = QLabel('FMA1700A')
+        self.all_label = QLabel('Pickoff Client')
         self.all_label.setFont(QFont(shell_font, pointSize=18))
         self.all_label.setAlignment(Qt.AlignCenter)
             # readout
-        self.flow_display_label = QLabel('Flow (%)')
-        self.flow_display = QLabel('Flow')
-        self.flow_display.setFont(QFont(shell_font, pointSize=25))
-        self.flow_display.setAlignment(Qt.AlignCenter)
-        self.flow_display.setStyleSheet('color: blue')
+        self.voltage_display_label = QLabel('VPP (V)')
+        self.voltage_display = QLabel('000.00')
+        self.voltage_display.setFont(QFont(shell_font, pointSize=25))
+        self.voltage_display.setAlignment(Qt.AlignCenter)
+        self.voltage_display.setStyleSheet('color: blue')
             # record button
         self.record_button = TextChangingButton(('Stop Recording', 'Start Recording'))
         self.record_button.setMaximumHeight(25)
@@ -35,11 +35,11 @@ class fma1700a_gui(QFrame):
         layout = QGridLayout(self)
         col1 = 0
         layout.addWidget(self.all_label, 0, col1)
-        layout.addWidget(self.flow_display_label, 1, col1)
-        layout.addWidget(self.flow_display, 2, col1)
+        layout.addWidget(self.voltage_display_label, 1, col1)
+        layout.addWidget(self.voltage_display, 2, col1)
         layout.addWidget(self.record_button, 3, col1)
 
 
 if __name__ == "__main__":
     from EGGS_labrad.clients import runGUI
-    runGUI(fma1700a_gui)
+    runGUI(pickoff_gui)
