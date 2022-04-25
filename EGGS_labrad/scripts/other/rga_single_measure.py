047c162634b71a4eb342133b9e006b54414f4670
Clayton Ho <claytonho@g.ucla.edu>
2022-04-24 23:42:22 -0700

    rga single measure script now works

--- EGGS_labrad/scripts/other/rga_single_measure.py
+++ EGGS_labrad/scripts/other/rga_single_measure.py
@@ -5,10 +5,8 @@ try:
     cxn = labrad.connect()
 
     # general import
-    from numpy import arange
-    from time import time
+    from numpy import arange, transpose
     from EGGS_labrad.clients import createTrunk
-    start_time = time()
 
     # get servers
     rga = cxn.rga_server
@@ -16,7 +14,7 @@ try:
     cr = cxn.context()
 
     # parameters
-    (mass_initial, mass_final, mass_steps) = (39, 41, 2)
+    (mass_initial, mass_final, mass_steps) = (1, 100, 10)
     dv_dep_var = [('Partial Pressures', 'Pressure', 'Torr')]
 
     # create dataset
@@ -34,10 +32,13 @@ try:
 
     # take rga data
     res = rga.scan_start('a', 1)
-    res = list(res[1])
-    res = [time() - start_time] + res
+    res_t = transpose(res)
     # print(res)
-    dv.add(res, context=cr)
+    dv.add(res_t, context=cr)
 
 except Exception as e:
     print('Error:', e)
+
+finally:
+    # turn off rga
+    rga.ionizer_filament(0)
