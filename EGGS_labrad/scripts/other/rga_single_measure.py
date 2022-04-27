c316affee6cefa8aa0b9f5f4ccde2946f26150a2
Clayton Ho <claytonho@g.ucla.edu>
2022-04-26 23:03:01 -0700

    updated rga single measure script to have better organization and documentation

--- EGGS_labrad/scripts/other/rga_single_measure.py
+++ EGGS_labrad/scripts/other/rga_single_measure.py
@@ -3,13 +3,13 @@ Automatically take and store a given number of RGA traces.
 """
 name_tmp = 'RGA Single Trace'
 (mass_initial, mass_final, mass_steps) = (1, 100, 10)
-num_traces = 2
+num_traces = 5
 
 try:
     # connect to labrad
     import labrad
     cxn = labrad.connect()
-
+    print('Connection successful.')
     # general import
     from numpy import arange, transpose
     from EGGS_labrad.clients import createTrunk
@@ -18,21 +18,21 @@ try:
     rga = cxn.rga_server
     dv = cxn.data_vault
     cr = cxn.context()
-
-    # parameters
-    dv_dep_var = [('Partial Pressures', 'Pressure', 'Torr')]
-
-    # create dataset
-    trunk_tmp = createTrunk(name_tmp)
-    dv.cd(trunk_tmp, True, context=cr)
+    print('Server connection successful.')
 
     # set up rga scan
     rga.scan_mass_initial(mass_initial)
     rga.scan_mass_final(mass_final)
     rga.scan_mass_steps(mass_steps)
-
-    # turn on rga
     rga.ionizer_filament('*')
+    print('RGA setup successful.')
+
+    # create dataset
+    dv_dep_var = [('Partial Pressures', 'Pressure', 'Torr')]
+    trunk_tmp = createTrunk(name_tmp)
+    dv.cd(trunk_tmp, True, context=cr)
+    print('Dataset successfully created')
+
 
     for i in range(num_traces):
         # create dataset
@@ -40,8 +40,8 @@ try:
         # take rga data
         res = rga.scan_start('a', 1)
         res_t = transpose(res)
-        # print(res)
         dv.add(res_t, context=cr)
+        print('Successfully added trace', i)
 
 except Exception as e:
     print('Error:', e)
