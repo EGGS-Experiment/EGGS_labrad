68e0a1a89746a67df44c47ed4b46def4fb7af154
Clayton Ho <claytonho@g.ucla.edu>
2022-05-07 16:15:38 -0700

    saving changes

--- EGGS_labrad/clients/ARTIQ_client/ARTIQ_client.py
+++ EGGS_labrad/clients/ARTIQ_client/ARTIQ_client.py
@@ -24,16 +24,8 @@ class ARTIQ_client(QMainWindow):
         LABRADHOST = os.environ['LABRADHOST']
         from labrad.wrappers import connectAsync
         self.cxn = yield connectAsync(LABRADHOST, name=self.name)
-        # check that required servers are online
-        try:
-            self.dv = yield self.cxn.data_vault
-            self.reg = yield self.cxn.registry
-        except Exception as e:
-            print(e)
-            raise
         return self.cxn
 
-    @inlineCallbacks
     def makeLayout(self, cxn):
         # central layout
         centralWidget = QWidget()
@@ -41,10 +33,10 @@ class ARTIQ_client(QMainWindow):
         self.tabWidget = QTabWidget()
         self.tabWidget.setMovable(True)
         # create subwidgets
-        ttl_widget = yield self.makeTTLWidget(self.reactor, cxn)
-        dds_widget = yield self.makeDDSWidget(self.reactor, cxn)
-        dac_widget = yield self.makeDACWidget(self.reactor, cxn)
-        adc_widget = yield self.makeADCWidget(self.reactor, cxn)
+        ttl_widget = self.makeTTLWidget(self.reactor, cxn)
+        dds_widget = self.makeDDSWidget(self.reactor, cxn)
+        dac_widget = self.makeDACWidget(self.reactor, cxn)
+        adc_widget = self.makeADCWidget(self.reactor, cxn)
         # create tabs for each subwidget
         self.tabWidget.addTab(ttl_widget, '&TTL')
         self.tabWidget.addTab(dds_widget, '&DDS')
@@ -57,57 +49,23 @@ class ARTIQ_client(QMainWindow):
 
     def makeTTLWidget(self, reactor, cxn):
         from EGGS_labrad.clients.ARTIQ_client.TTL_client import TTL_client
-        clients = {TTL_client: {"pos": (0, 0)}}
-        return self._createTabLayout(clients, reactor, cxn)
+        client_tmp = TTL_client(reactor, cxn)
+        return client_tmp
 
     def makeDDSWidget(self, reactor, cxn):
         from EGGS_labrad.clients.ARTIQ_client.DDS_client import DDS_client
-        clients = {DDS_client: {"pos": (0, 0)}}
-        return self._createTabLayout(clients, reactor, cxn)
+        client_tmp = DDS_client(reactor, cxn)
+        return client_tmp
 
     def makeDACWidget(self, reactor, cxn):
         from EGGS_labrad.clients.ARTIQ_client.DAC_client import DAC_client
-        clients = {DAC_client: {"pos": (0, 0)}}
-        return self._createTabLayout(clients, reactor, cxn)
+        client_tmp = DAC_client(reactor, cxn)
+        return client_tmp
 
     def makeADCWidget(self, reactor, cxn):
         from EGGS_labrad.clients.ARTIQ_client.ADC_client import ADC_client
-        clients = {ADC_client: {"pos": (0, 0)}}
-        return self._createTabLayout(clients, reactor, cxn)
-
-    def _createTabLayout(self, clientDict, reactor, cxn=None):
-        """
-        Creates a tab widget from constituent widgets stored in a dictionary.
-        """
-        holder_widget = QWidget()
-        holder_layout = QGridLayout(holder_widget)
-        for client, config in clientDict.items():
-            # get configuration settings for each client
-            position = config["pos"]
-            args, kwargs = ((), {})
-            try:
-                args = config["args"]
-            except KeyError:
-                pass
-            try:
-                kwargs = config["kwargs"]
-            except KeyError:
-                pass
-            # start up client
-            try:
-                client_tmp = client(reactor, cxn=cxn, *args, **kwargs)
-            except Exception as e:
-                print(client, e)
-            # try to add GUI to tabwidget
-            try:
-                if hasattr(client_tmp, 'getgui'):
-                    holder_layout.addWidget(client_tmp.getgui(), *position)
-                elif hasattr(client_tmp, 'gui'):
-                    print('hsagui')
-                    holder_layout.addWidget(client_tmp.gui, *position)
-            except Exception as e:
-                print(e)
-        return holder_widget
+        client_tmp = ADC_client(reactor, cxn)
+        return client_tmp
 
     def closeEvent(self, x):
         self.cxn.disconnect()
@@ -116,6 +74,29 @@ class ARTIQ_client(QMainWindow):
 
 
 if __name__ == "__main__":
-    from EGGS_labrad.clients import runClient
-    runClient(ARTIQ_client)
+    # set up logging
+    from sys import stdout
+    from twisted.python import log
+    log.startLogging(stdout)
+    # set up qapplication
+    app = QApplication([])
+    try:
+        import qt5reactor
+        qt5reactor.install()
+    except Exception as e:
+        print(e)
+    # instantiate client with a reactor
+    from twisted.internet import reactor
+    client_tmp = ARTIQ_client(reactor)
+    # show gui
+    client_tmp.showMaximized()
+    # start reactor
+    reactor.callWhenRunning(app.exec)
+    reactor.addSystemEventTrigger('after', 'shutdown', client_tmp.close)
+    reactor.runReturn()
+    # close client on exit
+    try:
+        client_tmp.close()
+    except Exception as e:
+        print(e)
 
