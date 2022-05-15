859acfc71b3eb2c36d26a185441d538088481b5d
Clayton Ho <claytonho@g.ucla.edu>
2022-05-15 04:04:01 -0700

    cleaned up RF GUI

--- EGGS_labrad/clients/trap_clients/RF_gui.py
+++ EGGS_labrad/clients/trap_clients/RF_gui.py
@@ -1,155 +1,128 @@
 from PyQt5.QtGui import QFont
-from PyQt5.QtCore import QRect, QCoreApplication, QMetaObject
-from PyQt5.QtWidgets import QWidget, QFrame, QLabel, QSizePolicy, QGridLayout, QPushButton, QDoubleSpinBox, QHBoxLayout
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QFrame, QLabel, QGridLayout, QPushButton, QDoubleSpinBox, QHBoxLayout, QVBoxLayout
 
-from EGGS_labrad.clients.Widgets import TextChangingButton
+from EGGS_labrad.clients.Widgets import Lockswitch, TextChangingButton, QCustomGroupBox
 
 
 class RF_gui(QFrame):
 
     def __init__(self):
         super().__init__()
-        self.setupUi()
+        self.setFixedSize(470, 250)
+        self.setFrameStyle(0x0001 | 0x0030)
+        self.setWindowTitle("RF Client")
+        self.makeLayout()
 
-    def setupUi(self):
-        RF_gui = self
-        RF_gui.setObjectName("RF_gui")
-        RF_gui.setFixedSize(470, 250)
-        RF_gui.setFrameStyle(0x0001 | 0x0030)
-        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
-        self.rf_label = QLabel(RF_gui)
-        self.rf_label.setGeometry(QRect(120, 10, 211, 41))
-        self.rf_label.setObjectName("rf_label")
-        self.layoutWidget = QWidget(RF_gui)
-        self.layoutWidget.setGeometry(QRect(10, 60, 121, 181))
-        self.layoutWidget.setObjectName("layoutWidget")
-        self.gridLayout = QGridLayout(self.layoutWidget)
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.gridLayout.setObjectName("gridLayout")
+    def _makeWaveformWidget(self):
+        waveform_widget = QWidget()
+        waveform_layout = QGridLayout(waveform_widget)
+        waveform_layout.setContentsMargins(0, 0, 0, 0)
+
+        wav_ampl_label = QLabel("Amplitude (dBm)")
+        wav_freq_label = QLabel("Frequency (MHz)")
+
         self.wav_toggle = TextChangingButton(('On', 'Off'))
-        # self.wav_toggle = QPushButton(self.layoutWidget)
-        # self.wav_toggle.setObjectName("wav_toggle")
-        self.gridLayout.addWidget(self.wav_toggle, 4, 0, 1, 1)
-        self.wav_ampl = QDoubleSpinBox(self.layoutWidget)
+        self.wav_reset = QPushButton("Reset")
+        self.wav_reset.setFont(QFont('MS Shell Dlg 2', pointSize=10))
+        self.wav_lockswitch = Lockswitch()
+
+        self.wav_ampl = QDoubleSpinBox()
         self.wav_ampl.setDecimals(1)
-        self.wav_ampl.setMinimum(-140.0)
-        self.wav_ampl.setMaximum(13.0)
+        self.wav_ampl.setRange(-140.0, 13.0)
         self.wav_ampl.setSingleStep(0.1)
-        self.wav_ampl.setObjectName("wav_ampl")
         self.wav_ampl.setKeyboardTracking(False)
-        self.gridLayout.addWidget(self.wav_ampl, 3, 0, 1, 1)
-        self.wav_freq = QDoubleSpinBox(self.layoutWidget)
+
+        self.wav_freq = QDoubleSpinBox()
         self.wav_freq.setDecimals(3)
-        self.wav_freq.setMinimum(0.009)
-        self.wav_freq.setMaximum(1040.0)
+        self.wav_freq.setRange(0.009, 1040.0)
         self.wav_freq.setSingleStep(0.001)
         self.wav_freq.setProperty("value", 9.0)
-        self.wav_freq.setObjectName("wav_freq")
         self.wav_freq.setKeyboardTracking(False)
-        self.gridLayout.addWidget(self.wav_freq, 1, 0, 1, 1)
-        self.wav_ampl_label = QLabel(self.layoutWidget)
-        self.wav_ampl_label.setObjectName("wav_ampl_label")
-        self.gridLayout.addWidget(self.wav_ampl_label, 2, 0, 1, 1)
-        self.wav_freq_label = QLabel(self.layoutWidget)
-        self.wav_freq_label.setObjectName("wav_freq_label")
-        self.gridLayout.addWidget(self.wav_freq_label, 0, 0, 1, 1)
-        self.wav_reset = QPushButton(self.layoutWidget)
-        self.wav_reset.setFont(QFont('MS Shell Dlg 2', pointSize=10))
-        self.wav_reset.setObjectName("wav_reset")
-        self.gridLayout.addWidget(self.wav_reset, 5, 0, 1, 1)
-        self.wav_lockswitch = TextChangingButton(('Locked', 'Unlocked'))
-        self.gridLayout.addWidget(self.wav_lockswitch, 6, 0, 1, 1)
-        self.mod_freq_label = QLabel(RF_gui)
-        self.mod_freq_label.setGeometry(QRect(210, 60, 169, 37))
-        self.mod_freq_label.setObjectName("mod_freq_label")
-        self.mod_freq = QDoubleSpinBox(RF_gui)
-        self.mod_freq.setGeometry(QRect(210, 100, 169, 20))
+
+        waveform_layout.addWidget(wav_freq_label,       0, 0, 1, 1)
+        waveform_layout.addWidget(self.wav_freq,        1, 0, 1, 1)
+        waveform_layout.addWidget(wav_ampl_label,       2, 0, 1, 1)
+        waveform_layout.addWidget(self.wav_ampl,        3, 0, 1, 1)
+        waveform_layout.addWidget(self.wav_toggle,      4, 0, 1, 1)
+        waveform_layout.addWidget(self.wav_reset,       5, 0, 1, 1)
+        waveform_layout.addWidget(self.wav_lockswitch,  6, 0, 1, 1)
+        return QCustomGroupBox(waveform_widget, "Waveform")
+
+    def _makeModulationWidget(self):
+        modulation_widget = QWidget()
+        modulation_layout = QGridLayout(modulation_widget)
+
+        # create modulation frequency subwidget
+        mod_freq_label = QLabel("Modulation Frequency (kHz)")
+        self.mod_freq = QDoubleSpinBox()
         self.mod_freq.setDecimals(1)
-        self.mod_freq.setMinimum(1)
-        self.mod_freq.setMaximum(500.0)
+        self.mod_freq.setRange(1, 500.0)
         self.mod_freq.setSingleStep(1.0)
-        self.mod_freq.setObjectName("mod_freq")
         self.mod_freq.setKeyboardTracking(False)
-        self.widget = QWidget(RF_gui)
-        self.widget.setGeometry(QRect(145, 150, 320, 91))
-        self.widget.setObjectName("widget")
-        self.horizontalLayout = QHBoxLayout(self.widget)
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.gridLayout_2 = QGridLayout()
-        self.gridLayout_2.setObjectName("gridLayout_2")
-        self.mod_ampl_toggle_label = QLabel(self.widget)
-        self.mod_ampl_toggle_label.setObjectName("mod_ampl_toggle_label")
-        self.gridLayout_2.addWidget(self.mod_ampl_toggle_label, 2, 0, 1, 1)
-        self.mod_ampl_depth = QDoubleSpinBox(self.widget)
+
+        # create modulation subwidgets
+        mod_ampl_depth_label = QLabel("Amplitude Depth (%)")
+        self.mod_ampl_depth = QDoubleSpinBox()
         self.mod_ampl_depth.setDecimals(1)
-        self.mod_ampl_depth.setMinimum(0)
-        self.mod_ampl_depth.setMaximum(100.0)
+        self.mod_ampl_depth.setRange(0, 100.0)
         self.mod_ampl_depth.setSingleStep(0.1)
-        self.mod_ampl_depth.setObjectName("mod_ampl_depth")
         self.mod_ampl_depth.setKeyboardTracking(False)
-        self.gridLayout_2.addWidget(self.mod_ampl_depth, 7, 0, 1, 1)
-        self.mod_ampl_depth_label = QLabel(self.widget)
-        self.mod_ampl_depth_label.setObjectName("mod_ampl_depth_label")
-        self.gridLayout_2.addWidget(self.mod_ampl_depth_label, 4, 0, 1, 1)
         self.mod_ampl_toggle = TextChangingButton(('On', 'Off'))
-        self.gridLayout_2.addWidget(self.mod_ampl_toggle, 3, 0, 1, 1)
-        self.horizontalLayout.addLayout(self.gridLayout_2)
-        self.gridLayout_3 = QGridLayout()
-        self.gridLayout_3.setObjectName("gridLayout_3")
-        self.mod_freq_toggle_label = QLabel(self.widget)
-        self.mod_freq_toggle_label.setObjectName("mod_freq_toggle_label")
-        self.gridLayout_3.addWidget(self.mod_freq_toggle_label, 2, 0, 1, 1)
-        self.mod_freq_dev = QDoubleSpinBox(self.widget)
+
+        mod_freq_dev_label = QLabel("Frequency Dev. (kHz)")
+        self.mod_freq_toggle = TextChangingButton(('On', 'Off'))
+        self.mod_freq_dev = QDoubleSpinBox()
         self.mod_freq_dev.setDecimals(2)
         self.mod_freq_dev.setMaximum(2000.0)
         self.mod_freq_dev.setSingleStep(0.01)
-        self.mod_freq_dev.setObjectName("mod_freq_dev")
         self.mod_freq_dev.setKeyboardTracking(False)
-        self.gridLayout_3.addWidget(self.mod_freq_dev, 7, 0, 1, 1)
-        self.mod_freq_dev_label = QLabel(self.widget)
-        self.mod_freq_dev_label.setObjectName("mod_freq_dev_label")
-        self.gridLayout_3.addWidget(self.mod_freq_dev_label, 4, 0, 1, 1)
-        self.mod_freq_toggle = TextChangingButton(('On', 'Off'))
-        self.gridLayout_3.addWidget(self.mod_freq_toggle, 3, 0, 1, 1)
-        self.horizontalLayout.addLayout(self.gridLayout_3)
-        self.gridLayout_4 = QGridLayout()
-        self.gridLayout_4.setObjectName("gridLayout_4")
-        self.mod_phase_toggle_label = QLabel(self.widget)
-        self.mod_phase_toggle_label.setObjectName("mod_phase_toggle_label")
-        self.gridLayout_4.addWidget(self.mod_phase_toggle_label, 2, 0, 1, 1)
-        self.mod_phase_dev = QDoubleSpinBox(self.widget)
+
+        mod_phase_dev_label = QLabel("Phase Dev. (rad)")
+        self.mod_phase_toggle = TextChangingButton(('On', 'Off'))
+        self.mod_phase_dev = QDoubleSpinBox()
         self.mod_phase_dev.setDecimals(3)
-        self.mod_phase_dev.setMinimum(0.0)
-        self.mod_phase_dev.setMaximum(400.0)
+        self.mod_phase_dev.setRange(0.0, 400.0)
         self.mod_phase_dev.setSingleStep(1)
         self.mod_phase_dev.setProperty("value", 0.0)
-        self.mod_phase_dev.setObjectName("mod_phase_dev")
         self.mod_phase_dev.setKeyboardTracking(False)
-        self.gridLayout_4.addWidget(self.mod_phase_dev, 7, 0, 1, 1)
-        self.mod_phase_dev_label = QLabel(self.widget)
-        self.mod_phase_dev_label.setObjectName("mod_phase_dev_label")
-        self.gridLayout_4.addWidget(self.mod_phase_dev_label, 4, 0, 1, 1)
-        self.mod_phase_toggle = TextChangingButton(('On', 'Off'))
-        self.gridLayout_4.addWidget(self.mod_phase_toggle, 3, 0, 1, 1)
-        self.horizontalLayout.addLayout(self.gridLayout_4)
-        self.retranslateUi(RF_gui)
-        QMetaObject.connectSlotsByName(RF_gui)
 
-    def retranslateUi(self, RF_gui):
-        _translate = QCoreApplication.translate
-        RF_gui.setWindowTitle(_translate("RF_gui", "RF Client"))
-        self.rf_label.setText(_translate("RF_gui", "<html><head/><body><p align=\"center\"><span style=\" font-size:26pt; vertical-align:super;\">Trap RF Client</span></p></body></html>"))
-        self.wav_ampl_label.setText(_translate("RF_gui", "Amplitude (dBm)"))
-        self.wav_freq_label.setText(_translate("RF_gui", "Frequency (MHz)"))
-        self.wav_reset.setText(_translate("RF_gui", "Reset"))
-        self.mod_freq_label.setText(_translate("RF_gui", "Modulation Frequency (kHz)"))
-        self.mod_ampl_toggle_label.setText(_translate("RF_gui", "Amplitude Mod."))
-        self.mod_ampl_depth_label.setText(_translate("RF_gui", "Amplitude Depth (%)"))
-        self.mod_freq_toggle_label.setText(_translate("RF_gui", "Frequency Mod."))
-        self.mod_freq_dev_label.setText(_translate("RF_gui", "Frequency Dev. (kHz)"))
-        self.mod_phase_toggle_label.setText(_translate("RF_gui", "Phase Mod."))
-        self.mod_phase_dev_label.setText(_translate("RF_gui", "Phase Dev. (rad)"))
+        def _createMiniWidget(subwidget_list, title):
+            widget = QWidget()
+            layout = QVBoxLayout(widget)
+            for subwidget in subwidget_list:
+                layout.addWidget(subwidget)
+            return QCustomGroupBox(widget, title)
+
+        am_widget = _createMiniWidget([self.mod_ampl_toggle, mod_ampl_depth_label, self.mod_ampl_depth], "Amplitude Modulation")
+        fm_widget = _createMiniWidget([self.mod_freq_toggle, mod_freq_dev_label, self.mod_freq_dev], "Frequency Modulation")
+        pm_widget = _createMiniWidget([self.mod_phase_toggle, mod_phase_dev_label, self.mod_phase_dev], "Phase Modulation")
+
+        modulation_layout.addWidget(mod_freq_label,             0, 1, 1, 4)
+        modulation_layout.addWidget(self.mod_freq,              1, 1, 1, 4)
+        modulation_layout.addWidget(am_widget,                  2, 0, 3, 2)
+        modulation_layout.addWidget(fm_widget,                  2, 2, 3, 2)
+        modulation_layout.addWidget(pm_widget,                  2, 4, 3, 2)
+
+        return QCustomGroupBox(modulation_widget, "Modulation")
+
+    def makeLayout(self):
+        font = QFont('MS Shell Dlg 2', pointSize=20)
+        layout = QGridLayout(self)
+
+        # title
+        title = QLabel("Trap RF Client")
+        title.setFont(font)
+        title.setAlignment(Qt.AlignCenter)
+
+        # make widgets
+        waveform_widget_wrapped = self._makeWaveformWidget()
+        modulation_widget_wrapped = self._makeModulationWidget()
+
+        # lay out
+        layout.addWidget(title,                             0, 0, 1, 4)
+        layout.addWidget(waveform_widget_wrapped,           1, 0, 6, 1)
+        layout.addWidget(modulation_widget_wrapped,         1, 1, 5, 3)
 
 
 if __name__ == "__main__":
