326ab0a5f672193f722caff92b5bc207c8edf1f1
Clayton Ho <claytonho@g.ucla.edu>
2022-06-06 20:56:58 -0700

    tested ionchain and added test behavior

--- EGGS_labrad/clients/stability_client/ionChain.py
+++ EGGS_labrad/clients/stability_client/ionChain.py
@@ -58,7 +58,7 @@ class ionChain(object):
         self.k_dc = k_dc
         self.z0 = z0
         # chain/mode variables
-        self.lengthscale = self._length_scale()
+        self.l0 = self._length_scale()
         self.mathieu_order = 1
         self.equilibrium_positions = {}
         self.mode_axial = {}
@@ -79,15 +79,18 @@ class ionChain(object):
         # add ion to end of chain by default
         if position is None:
             position = len(self.ions)
+            mass *= _AMU
         # otherwise check that position exists
         elif position not in range(len(self.ions)):
-            self.ions[position] = mass * _AMU
-        else:
             raise Exception("Error: ion position exceeds chain length.")
 
         # create ion and insert into chain
         ion = ionObject(mass)
         self.ions.insert(position, ion)
+
+        # calculate secular frequencies
+        ion.secular_axial = self._axial_secular_frequency(mass)
+        ion.secular_radial = self._radial_secular_frequency(mass)
 
         # recalculate chain values
         self._calculate_equilibrium_positions()
@@ -95,18 +98,17 @@ class ionChain(object):
 
     def set_ion(self, mass, position):
         """
-        Sets the mass of an ion within the chain.
+        Changes the mass of an ion within the chain.
         Arguments:
             mass        (float)     : the mass of the ion (in amu).
             position    (position)  : the position (i.e. index) of the ion. Starts at 0.
         """
         # make sure position is in array
-        if position not in len(self.ions):
-            self.ions[position] = mass * _AMU
-        else:
+        if position not in range(len(self.ions)):
             raise Exception("Error: ion position exceeds chain length.")
 
         # change ion mass
+        mass *= _AMU
         ion = self.ions[position]
         ion.mass = mass
 
@@ -141,7 +143,7 @@ class ionChain(object):
         Returns:
                     (float) : the axial secular frequency (in Hz).
         """
-        return np.sqrt((2 * _QE * self.k_dc * self.v_dc) / (mass * np.pow(self.z0, 2)))
+        return np.sqrt((2 * _QE * self.k_dc * self.v_dc) / (mass * self.z0**2))
 
     def _radial_secular_frequency(self, mass):
         """
@@ -151,7 +153,8 @@ class ionChain(object):
         Returns:
                     (float) : the axial secular frequency (in Hz).
         """
-        return np.sqrt((2 * _QE * self.k_dc * self.v_dc) / (mass * np.pow(self.z0, 2)))
+        return np.sqrt(0.5 * ((_QE * self.v_rf * self.k_rf) / (mass * self.w_rf * self.r0**2))**2 -
+                       ((_QE * self.k_dc * self.v_dc) / (mass * self.z0**2)))
 
     def _length_scale(self):
         """
@@ -220,7 +223,7 @@ class ionChain(object):
         wzMat *= np.outer(wsecArrAxial, np.ones(num_ions)) ** 2
 
         # get anharmonicities
-        ah = np.diag(np.power(wsecArrRadial / wsecArrAxial), 2)
+        ah = np.diag(np.power(wsecArrRadial / wsecArrAxial, 2))
 
         # create hessian matrices
         hessMatAxial = wzMat * (np.identity(num_ions) + 2 * (distSumMat - distMat))
@@ -237,5 +240,45 @@ class ionChain(object):
         eigenvalsRadial, eigenvecRadial = np.sqrt(eigenvalsRadial), eigenvecRadial.transpose()
         radialModes = dict(zip(eigenvalsRadial, eigenvecRadial))
         self.mode_radial = radialModes
-
+        # todo: sort increasing by mode freq
         return axialModes, radialModes
+
+
+if __name__ == "__main__":
+    # create ion chain object
+    # MOTion trap
+    chain = ionChain(v_rf=270.2, w_rf=1.697*_wmhz, r0=6.8453e-3, v_dc=94.05, k_dc=0.022, z0=10.16e-3)
+
+    # create ions
+    massList = [38, 40, 38, 40, 40]
+    for mass_amu in massList:
+        chain.insert_ion(mass_amu)
+
+    print("Equilibrium Positions:", np.round(chain.equilibrium_positions, 3))
+    print("Axial Modes:")
+    print("\tMode Freq. (x2\u03C0 MHz)\tMode Eigenvectors")
+    for mode_freq, mode_vec in chain.mode_axial.items():
+        print("\t{:.3f}".format(mode_freq / _wmhz), "\t\t", mode_vec)
+
+    print("Radial Modes:")
+    print("\tMode Freq. (x2\u03C0 MHz)\tMode Eigenvectors")
+    for mode_freq, mode_vec in chain.mode_radial.items():
+        print("\t{:.3f}".format(mode_freq / _wmhz), "\t\t", mode_vec)
+
+    print("\n"
+          "\n"
+          "\n")
+
+    chain.set_ion(30, 0)
+    chain.set_ion(30, 2)
+
+    print("Equilibrium Positions:", np.round(chain.equilibrium_positions, 3))
+    print("Axial Modes:")
+    print("\tMode Freq. (x2\u03C0 MHz)\tMode Eigenvectors")
+    for mode_freq, mode_vec in chain.mode_axial.items():
+        print("\t{:.3f}".format(mode_freq / _wmhz), "\t\t", mode_vec)
+
+    print("Radial Modes:")
+    print("\tMode Freq. (x2\u03C0 MHz)\tMode Eigenvectors")
+    for mode_freq, mode_vec in chain.mode_radial.items():
+        print("\t{:.3f}".format(mode_freq / _wmhz), "\t\t", mode_vec)
