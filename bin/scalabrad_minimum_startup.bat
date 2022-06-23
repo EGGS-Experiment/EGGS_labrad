b514e6c915f5b88ae1ae13c744aba68204572603
Clayton Ho <claytonho@g.ucla.edu>
2022-06-23 02:38:02 -0700

    added minimum startup batch files for scalabrad and scalabrad-web server

--- bin/scalabrad_minimum_startup.bat
+++ bin/scalabrad_minimum_startup.bat
@@ -1,16 +1,11 @@
-@echo off
+@ECHO OFF
+@SETLOCAL
 
-SET ERROR_CODE=0
-@setlocal
-
-SET JAVA_EXE="%JAVA_HOME%\bin\java.exe"
 SET CMD_LINE_ARGS=%*
 SET PROG_HOME=%~dp0..
 
-SET CMDLINE=%JAVA_EXE%  -cp "%PROG_HOME%\lib\*;" -Dprog.home="%PROG_HOME%" -Dprog.version="0.8.3" org.labrad.manager.Manager %CMD_LINE_ARGS%
-%CMDLINE%
+"%JAVA_HOME%\bin\java.exe" -cp "%PROG_HOME%\lib\*;" -Dprog.home="%PROG_HOME%" -Dprog.version="0.8.3" org.labrad.manager.Manager %CMD_LINE_ARGS%
 
-@endlocal
-if ERRORLEVEL 1 (set ERROR_CODE=1)
+@ENDLOCAL
 
-exit /B %ERROR_CODE%
+EXIT /B
