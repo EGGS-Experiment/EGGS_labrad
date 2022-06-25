a2d401eba13d30b52fbaddddfc1a5e2fae0db1c2
Clayton Ho <claytonho@g.ucla.edu>
2022-06-25 02:26:49 -0700

    made updates/cleanups to startup batch files

--- bin/utils/start_labrad_experiments.bat
+++
@@ -1,8 +0,0 @@
-::Starts all LabRAD servers needed for experiments
-
-@ECHO OFF
-
-REM: Servers
-START "Labrad Data Vault" /min CMD "/k activate labart && python %EGGS_LABRAD_ROOT%\EGGS_labrad\servers\data_vault.py"
-START "Labrad Parameter Vault" /min CMD "/k activate labart && python %EGGS_LABRAD_ROOT%\EGGS_labrad\servers\parameter_vault\parameter_vault.py"
-START "Labrad Script Scanner" /min CMD "/k activate labart && python %EGGS_LABRAD_ROOT%\EGGS_labrad\servers\script_scanner\script_scanner.py"
