f81b0ed1419ee70725bd52f5bd95956bf966b275
Clayton Ho <claytonho@g.ucla.edu>
2022-06-27 03:10:34 -0700

    fixed scalabrad startup files; now works 100%

--- bin/scalabrad-web_minimum_startup.bat
+++ bin/scalabrad-web_minimum_startup.bat
@@ -5,9 +5,10 @@
 @REM: todo either change prog_home or add scalabrad packages to this folder
 SET CMD_LINE_ARGS=%*
 SET PROG_HOME=%~dp0
+SET JAVA_EXE="%JAVA_HOME%\bin\java.exe"
 
-"%JAVA_HOME%\bin\java.exe" -cp "%PROG_HOME%\lib\*;" -Dprog.home="%PROG_HOME%" -Dprog.version="2.0.6" org.labrad.browser.WebServer %CMD_LINE_ARGS%
-
+SET CMDLINE=%JAVA_EXE% -cp "%PROG_HOME%lib\*;" -Dprog.home="%PROG_HOME%..\bin" -Dprog.version="2.0.6" org.labrad.browser.WebServer %CMD_LINE_ARGS%
+%CMDLINE%
 @ENDLOCAL
 
 EXIT /B
--- bin/scalabrad_minimum_startup.bat
+++ bin/scalabrad_minimum_startup.bat
@@ -3,8 +3,10 @@
 
 SET CMD_LINE_ARGS=%*
 SET PROG_HOME=%~dp0
+SET JAVA_EXE="%JAVA_HOME%\bin\java.exe"
 
-"%JAVA_HOME%\bin\java.exe" -cp "%PROG_HOME%\lib\*;" -Dprog.home="%PROG_HOME%" -Dprog.version="0.8.3" org.labrad.manager.Manager %CMD_LINE_ARGS%
+SET CMDLINE=%JAVA_EXE% -cp "%PROG_HOME%lib\*;" -Dprog.home="%PROG_HOME%..\bin" -Dprog.version="0.8.3" org.labrad.manager.Manager %CMD_LINE_ARGS%
+%CMDLINE%
 
 @ENDLOCAL
 
