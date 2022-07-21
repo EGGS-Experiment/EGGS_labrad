e907ad0d10051f2e6711f617b65f567f2acf28f5
Clayton Ho <claytonho@g.ucla.edu>
2022-07-21 01:11:52 -0700

    made more implementation updates to pmt client

--- EGGS_labrad/clients/PMT_client/PMT_client.py
+++ EGGS_labrad/clients/PMT_client/PMT_client.py
@@ -1,7 +1,8 @@
+from time import time
 from twisted.internet.task import LoopingCall
 from twisted.internet.defer import inlineCallbacks
 
-from EGGS_labrad.clients import GUIClient
+from EGGS_labrad.clients import GUIClient, createTrunk
 from EGGS_labrad.clients.PMT_client.PMT_gui import PMT_gui
 
 
@@ -19,6 +20,7 @@ class PMT_client(GUIClient):
         # set recording stuff
         self.c_record = self.cxn.context()
         self.recording = False
+        self.starttime = 0
         # create polling loop
         self.refresher = LoopingCall(self.update_counts)
 
@@ -41,10 +43,19 @@ class PMT_client(GUIClient):
     # SLOTS
     @inlineCallbacks
     def update_counts(self):
+        """
+        Main function that actually gets counts from artiq.
+        """
+        # get values from GUI
         sample_time_us = int(self.gui.sample_time.value())
         num_samples = int(self.gui.sample_num.value())
+        # get counts
         counts = yield self.aq.ttl_counter('ttl_counter{:d}'.format(0), sample_time_us, num_samples)
+        # update display
         self.gui.count_display.setText("{:.3f}".format(counts))
+        # store data if recording
+        if self.recording:
+            yield self.dv.add(time() - self.starttime, counts, context=self.c_record)
 
     def toggle_polling(self, status):
         # start if not running
@@ -52,14 +63,28 @@ class PMT_client(GUIClient):
             poll_interval_s = self.gui.poll_interval.value()
             self.gui.poll_interval.setEnabled(False)
             self.gui.read_once_switch.setEnabled(False)
-            self.count_display.setStyleSheet('color: green')
+            self.gui.count_display.setStyleSheet('color: green')
             self.refresher.start(poll_interval_s, now=True)
         # stop if running
         elif (not status) and (self.refresher.running):
             self.refresher.stop()
             self.gui.read_once_switch.setEnabled(True)
             self.gui.poll_interval.setEnabled(True)
-            self.count_display.setStyleSheet('color: red')
+            self.gui.count_display.setStyleSheet('color: red')
+
+    @inlineCallbacks
+    def record(self, status):
+        """
+        Creates a new dataset to record counts
+        tells polling loop to add data to data vault.
+        """
+        self.recording = status
+        # set up datavault
+        if self.recording:
+            self.starttime = time()
+            trunk = createTrunk(self.name)
+            yield self.dv.cd(trunk, True, context=self.c_record)
+            yield self.dv.new('PMT Counter', [('Elapsed time', 't')], [('PMT', 'Counts', 'Num.')], context=self.c_record)
 
     def _lock(self, status):
         self.gui.read_once_switch.setEnabled(status)
