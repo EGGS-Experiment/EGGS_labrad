4ef4bf56e75b2abca1bc8033f98b5ca578bc88ef
Clayton Ho <claytonho@g.ucla.edu>
2022-08-22 18:26:14 -0700

    created network analyzers and good spectrum analyzer

---
+++ EGGS_labrad/servers/network_analyzers/networkanalyzer_server.py
@@ -0,0 +1,372 @@
+"""
+### BEGIN NODE INFO
+[info]
+name = Network Analyzer Server
+version = 1.1.0
+description = Talks to network analyzers.
+
+[startup]
+cmdline = %PYTHON% %FILE%
+timeout = 20
+
+[shutdown]
+message = 987654321
+timeout = 20
+### END NODE INFO
+"""
+from time import sleep
+from labrad.server import setting
+from labrad.gpib import GPIBManagedServer
+
+# import device wrappers
+from RigolDSA800 import RigolDSA800Wrapper
+
+
+class NetworkAnalyzerServer(GPIBManagedServer):
+    """
+    Manages communication with all network analyzers.
+    """
+
+    name = 'Network Analyzer Server'
+
+    deviceWrappers = {
+        'Rigol Technologies DSA815': RigolDSA800Wrapper
+    }
+
+
+    # SYSTEM
+    @setting(11, "Reset", returns='')
+    def reset(self, c):
+        """
+        Reset the network analyzer to factory settings.
+        """
+        dev = self.selectedDevice(c)
+        yield dev.reset()
+        sleep(5)
+
+    @setting(12, "Clear Buffers", returns='')
+    def clear_buffers(self, c):
+        """
+        Clear device status buffers.
+        """
+        dev = self.selectedDevice(c)
+        yield dev.clear_buffers()
+
+    @setting(13, "Autoset", returns='')
+    def autoset(self, c):
+        """
+        Automatically set the amplitude and frequency to
+        view the dominant signal.
+        """
+        dev = self.selectedDevice(c)
+        yield dev.autoset()
+
+
+    # ATTENUATION
+    @setting(21, "Preamplifier", status=['b', 'i'], returns='b')
+    def preamplifier(self, c, status=None):
+        """
+        Enable/disable the preamplifier.
+        Arguments:
+            status  (bool): the status of the preamplifier.
+        Returns:
+                    (bool): the status of the preamplifier.
+        """
+        if type(status) == int:
+            if status not in (0, 1):
+                raise Exception('Error: input must be a boolean, 0, or 1.')
+        return self.selectedDevice(c).preamplifier(status)
+
+    @setting(22, "Attenuation", att='v', returns='v')
+    def attenuation(self, c, att=None):
+        """
+        Get/set the attenuation of the RF attenuator.
+        Arguments:
+            att     (float): the channel attenuation (in dB) from the RF attenuator.
+        Returns:
+                    (float): the channel attenuation (in dB) from the RF attenuator.
+        """
+        return self.selectedDevice(c).attenuation(att)
+
+
+    # FREQUENCY RANGE
+    @setting(111, "Frequency Start", freq='v', returns='v')
+    def frequencyStart(self, c, freq=None):
+        """
+        Get/set the start frequency.
+        Arguments:
+            freq    (float): the start frequency (in Hz).
+        Returns:
+                    (float): the start frequency (in Hz).
+        """
+        return self.selectedDevice(c).frequencyStart(freq)
+
+    @setting(112, "Frequency Stop", freq='v', returns='v')
+    def frequencyStop(self, c, freq=None):
+        """
+        Get/set the stop frequency.
+        Arguments:
+            freq    (float): the stop frequency (in Hz).
+        Returns:
+                    (float): the stop frequency (in Hz).
+        """
+        return self.selectedDevice(c).frequencyStop(freq)
+
+    @setting(113, "Frequency Center", freq='v', returns='v')
+    def frequencyCenter(self, c, freq=None):
+        """
+        Get/set center frequency.
+        Arguments:
+            freq    (float): the center frequency (in Hz).
+        Returns:
+                    (float): the center frequency (in Hz).
+        """
+        return self.selectedDevice(c).frequencyCenter(freq)
+
+    @setting(114, "Frequency Span", span='v', returns='v')
+    def frequencySpan(self, c, span=None):
+        """
+        Get/set the frequency span.
+        Arguments:
+            span    (float): the frequency span (in Hz).
+        Returns:
+                    (float): the frequency span (in Hz).
+        """
+        return self.selectedDevice(c).frequencySpan(span)
+
+
+    # AMPLITUDE
+    @setting(211, "Amplitude Reference", ampl='v', returns='v')
+    def amplitudeReference(self, c, ampl=None):
+        """
+        Get/set the amplitude reference level (i.e. the top of the trace).
+        Arguments:
+            ampl    (float): the reference level (in dBm).
+        Returns:
+                    (float): the reference level (in dBm).
+        """
+        return self.selectedDevice(c).amplitudeReference(ampl)
+
+    @setting(212, "Amplitude Offset", ampl='v', returns='v')
+    def amplitudeOffset(self, c, ampl=None):
+        """
+        Get/set the amplitude offset level. #todo better note and understand
+        Arguments:
+            ampl    (float): the offset level (in dBm).
+        Returns:
+                    (float): the offset level (in dBm).
+        """
+        return self.selectedDevice(c).amplitudeOffset(ampl)
+
+    @setting(213, "Amplitude Scale", factor='v', returns='v')
+    def amplitudeScale(self, c, factor=None):
+        """
+        Get/set the amplitude scale.
+        Arguments:
+            factor  (float): the amplitude scale (in dBm/div).
+        Returns:
+                    (float): the amplitude scale (in dBm/div).
+        """
+        return self.selectedDevice(c).amplitudeScale(factor)
+
+
+    # MARKER SETUP
+    @setting(311, "Marker Toggle", channel='i', status=['b', 'i'], returns='b')
+    def markerToggle(self, c, channel, status=None):
+        """
+        Enable/disable a marker channel.
+        Arguments:
+            channel (int): the marker channel to get/set.
+            status  (bool): whether the marker is on/off.
+        Returns:
+                    (bool): whether the marker is on/off.
+        """
+        if type(status) == int:
+            if status not in (0, 1):
+                raise Exception('Error: input must be a boolean, 0, or 1.')
+        return self.selectedDevice(c).markerToggle(channel, status)
+
+    @setting(312, "Marker Trace", channel='i', trace='i', returns='i')
+    def markerTrace(self, c, channel, trace=None):
+        """
+        Get/set the trace for a marker channel to follow.
+        Arguments:
+            channel (int): the marker channel to get/set.
+            status  (bool): whether the marker is on/off.
+        Returns:
+                    (bool): whether the marker is on/off.
+        """
+        return self.selectedDevice(c).markerTrace(channel, trace)
+
+    @setting(313, "Marker Mode", channel='i', mode='i', returns='i')
+    def markerMode(self, c, channel, mode=None):
+        """
+        Get/set the marker mode.
+            0: Position (normal).
+            1: Delta
+            2: Delta Pair
+            3: Span Pair
+        Arguments:
+            channel (int): the marker channel to get/set.
+            mode    (int): the marker mode, can be one of (0, 1, 2, 3).
+        Returns:
+                    (int): the marker mode.
+        """
+        return self.selectedDevice(c).markerMode(channel, mode)
+
+    @setting(314, "Marker Readout Mode", channel='i', mode='i', returns='i')
+    def markerReadoutMode(self, c, channel, mode=None):
+        """
+        Get/set the readout mode of a marker channel.
+            0: Frequency
+            1: Time
+            2: Inverse time
+            3: Period
+        Arguments:
+            channel (int): the marker channel to get/set.
+            mode    (int): the readout mode of the channel.
+        Returns:
+                    (int): the readout mode of the channel.
+        """
+        return self.selectedDevice(c).markerReadoutMode(channel, mode)
+
+    @setting(315, "Marker Track", channel='i', status=['b', 'i'], returns='b')
+    def markerTrack(self, c, channel, status=None):
+        """
+        Get/set the status of signal tracking.
+        If a marker is already active, signal tracking will
+        use that marker to set the center frequency.
+        If a marker does not already exist, signal tracking
+        will create a marker and use it to set the center frequency.
+        Arguments:
+            status  (bool): the status of signal tracking.
+        Returns:
+                    (bool): the status of signal tracking.
+        """
+        if type(status) == int:
+            if status not in (0, 1):
+                raise Exception('Error: input must be a boolean, 0, or 1.')
+        return self.selectedDevice(c).markerTrack(channel, status)
+
+
+    # MARKER READOUT
+    @setting(321, "Marker Amplitude", channel='i', returns='v')
+    def markerAmplitude(self, c, channel):
+        """
+        Get the value of a marker channel.
+        Arguments:
+            channel (int): the marker channel to get/set.
+        Returns:
+                    (float): the amplitude of the marker (in dBm).
+        """
+        return self.selectedDevice(c).markerAmplitude(channel)
+
+    @setting(322, "Marker Frequency", channel='i', freq='v', returns='v')
+    def markerFrequency(self, c, channel, freq=None):
+        """
+        Get/set the x-position of a normal mode marker.
+        Arguments:
+            channel (int): the marker channel to get/set.
+            freq    (float): the frequency of the marker.
+        Returns:
+                    (float): the frequency of the marker.
+        """
+        return self.selectedDevice(c).markerFrequency(channel, freq)
+
+
+    # PEAK
+    @setting(411, "Peak Search", status=['b', 'i'], returns='b')
+    def peakSearch(self, c, status=None):
+        """
+        Get/set the status of signal tracking.
+        If a marker is already active, signal tracking will
+        use that marker to set the center frequency.
+        If a marker does not already exist, signal tracking
+        will create a marker and use it to set the center frequency.
+
+        Arguments:
+            status  (bool): the status of signal tracking.
+        Returns:
+                    (bool): the status of signal tracking.
+        """
+        if type(status) == int:
+            if status not in (0, 1):
+                raise Exception('Error: input must be a boolean, 0, or 1.')
+        return self.selectedDevice(c).peakSearch(status)
+
+    @setting(412, "Peak Set", status='v', returns='b')
+    def peakSet(self, c, status=None):
+        """
+        Arguments:
+            status  (bool): the status of signal tracking.
+        Returns:
+                    (bool): the status of signal tracking.
+        """
+        # todo
+        return self.selectedDevice(c).peakSet(status)
+
+    @setting(421, "Peak Next", status='v', returns='b')
+    def peakNext(self, c, status=None):
+        """
+        Arguments:
+            status  (bool): the status of signal tracking.
+        Returns:
+                    (bool): the status of signal tracking.
+        """
+        # todo
+        return self.selectedDevice(c).peakNext(status)
+
+
+    # BANDWIDTH
+    @setting(511, "Bandwidth Sweep Time", time='v', returns='v')
+    def bandwidthSweepTime(self, c, time=None):
+        """
+        Get/set the sweep time.
+        Arguments:
+            time    (float): the sweep time (in s).
+        Returns:
+                    (float): the sweep time (in s).
+        """
+        return self.selectedDevice(c).bandwidthSweepTime(time)
+
+    @setting(521, "Bandwidth Resolution", bw='i', returns='i')
+    def bandwidthResolution(self, c, bw=None):
+        """
+        Get/set the resolution bandwidth.
+        Increasing resolution bandwidth increases the sweep time.
+        Arguments:
+            bw      (int): the resolution bandwidth (in Hz).
+        Returns:
+                    (int): the resolution bandwidth (in Hz).
+        """
+        return self.selectedDevice(c).bandwidthResolution(bw)
+
+    @setting(522, "Bandwidth Video", bw='i', returns='i')
+    def bandwidthVideo(self, c, bw=None):
+        """
+        Get/set the video bandwidth.
+        Increasing video bandwidth increases the sweep time.
+        Arguments:
+            bw      (int): the video bandwidth (in Hz).
+        Returns:
+                    (int): the video bandwidth (in Hz).
+        """
+        return self.selectedDevice(c).bandwidthVideo(bw)
+
+
+    # TRACE
+    @setting(911, "Trace", channel='i', returns='(*v*v)')
+    def getTrace(self, c, channel):
+        """
+        Get the current trace.
+        Arguments:
+            channel (int): the trace channel to get/set.
+        Returns:
+                    (*v, *v): the trace.
+        """
+        return self.selectedDevice(c).getTrace(channel)
+
+
+if __name__ == '__main__':
+    from labrad import util
+    util.runServer(NetworkAnalyzerServer())
---
+++ EGGS_labrad/servers/spectrum_analyzers/AgilentN9010A.py
@@ -0,0 +1,254 @@
+import numpy as np
+from labrad.gpib import GPIBDeviceWrapper
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+
+class RigolDSA800Wrapper(GPIBDeviceWrapper):
+
+    # SYSTEM
+    @inlineCallbacks
+    def reset(self):
+        yield self.write('*RST')
+
+    @inlineCallbacks
+    def clear_buffers(self):
+        yield self.write('*CLS')
+
+    @inlineCallbacks
+    def autoset(self):
+        yield self.write(':SENS:POW:ATUN')
+
+
+    # ATTENUATION
+    @inlineCallbacks
+    def preamplifier(self, status):
+        if status is not None:
+            yield self.write(':SENS:POW:RF:GAIN:STAT {:d}'.format(status))
+        resp = yield self.query(':SENS:POW:RF:GAIN:STAT?')
+        returnValue(bool(int(resp)))
+
+    @inlineCallbacks
+    def attenuation(self, att):
+        if att is not None:
+            if (att > 0) and (att < 30):
+                yield self.write(':SENS:POW:RF:ATT {:f}'.format(att))
+            else:
+                raise Exception('Error: RF attenuation must be in range: [0, 30].')
+        resp = yield self.query(':SENS:POW:RF:ATT?')
+        returnValue(float(resp))
+
+
+    # FREQUENCY RANGE
+    @inlineCallbacks
+    def frequencyStart(self, freq):
+        if freq is not None:
+            if (freq > 0) and (freq < 7.5e9):
+                yield self.write(':SENS:FREQ:STAR {:f}'.format(freq))
+            else:
+                raise Exception('Error: start frequency must be in range: [0, 7.5e9].')
+        resp = yield self.query(':SENS:FREQ:STAR?')
+        returnValue(float(resp))
+
+    @inlineCallbacks
+    def frequencyStop(self, freq):
+        if freq is not None:
+            if (freq > 0) and (freq < 7.5e9):
+                yield self.write(':SENS:FREQ:STOP {:f}'.format(freq))
+            else:
+                raise Exception('Error: stop frequency must be in range: [0, 7.5e9].')
+        resp = yield self.query(':SENS:FREQ:STOP?')
+        returnValue(float(resp))
+
+    @inlineCallbacks
+    def frequencyCenter(self, freq):
+        if freq is not None:
+            if (freq > 0) and (freq < 7.5e9):
+                yield self.write(':SENS:FREQ:CENT {:f}'.format(freq))
+            else:
+                raise Exception('Error: center frequency must be in range: [0, 7.5e9].')
+        resp = yield self.query(':SENS:FREQ:CENT?')
+        returnValue(float(resp))
+
+    @inlineCallbacks
+    def frequencySpan(self, span):
+        if span is not None:
+            if (span > 0) and (span < 7.5e9):
+                yield self.write(':SENS:FREQ:SPAN {:f}'.format(span))
+            else:
+                raise Exception('Error: frequency span must be in range: [0, 7.5e9].')
+        resp = yield self.query(':SENS:FREQ:SPAN?')
+        returnValue(float(resp))
+
+
+    # AMPLITUDE
+    @inlineCallbacks
+    def amplitudeReference(self, ampl):
+        if ampl is not None:
+            if (ampl > -100) and (ampl < 20):
+                yield self.write(':DISP:WIN:TRAC:Y:SCAL:RLEV {:f}'.format(ampl))
+            else:
+                raise Exception('Error: display reference value must be in range: [-100, 20].')
+        resp = yield self.query(':DISP:WIN:TRAC:Y:SCAL:RLEV?')
+        returnValue(float(resp))
+
+    @inlineCallbacks
+    def amplitudeOffset(self, ampl):
+        if ampl is not None:
+            if (ampl > -300) and (ampl < 300):
+                yield self.write(':DISP:WIN:TRAC:Y:SCAL:RLEV:OFFS {:f}'.format(ampl))
+            else:
+                raise Exception('Error: display offset must be in range: [-300, 300].')
+        resp = yield self.query(':DISP:WIN:TRAC:Y:SCAL:RLEV:OFFS?')
+        returnValue(float(resp))
+
+    @inlineCallbacks
+    def amplitudeScale(self, factor):
+        if factor is not None:
+            if (factor > 0.1) and (factor < 20):
+                yield self.write(':DISP:WIN:TRAC:Y:SCAL:PDIV {:f}'.format(factor))
+            else:
+                raise Exception('Error: display scale must be in range: [0.1, 20].')
+        resp = yield self.query(':DISP:WIN:TRAC:Y:SCAL:PDIV?')
+        returnValue(float(resp))
+
+
+    # MARKER SETUP
+    @inlineCallbacks
+    def markerToggle(self, channel, status):
+        if status is not None:
+            yield self.write(':CALC:MARK{:d}:STAT {:d}'.format(channel, status))
+        resp = yield self.query(':CALC:MARK{:d}:STAT?'.format(channel))
+        returnValue(bool(int(resp)))
+
+    @inlineCallbacks
+    def markerTrace(self, channel, trace):
+        if trace is not None:
+            yield self.write(':CALC:MARK{:d}:TRAC {:d}'.format(channel, trace))
+        resp = yield self.query(':CALC:MARK{:d}:TRAC?'.format(channel))
+        returnValue(int(resp))
+
+    @inlineCallbacks
+    def markerMode(self, channel, mode):
+        modeConvert = {0: 'POS', 1: 'DELT', 2: 'BAND', 3: 'SPAN'}
+        modeInvert = {val: key for key, val in modeConvert.items()}
+        if mode is not None:
+            mode = modeConvert[mode]
+            yield self.write(':CALC:MARK{:d}:MODE {:s}'.format(channel, mode))
+        resp = yield self.query(':CALC:MARK{:d}:MODE?'.format(channel))
+        returnValue(modeInvert[resp])
+
+    @inlineCallbacks
+    def markerReadoutMode(self, channel, mode):
+        modeConvert = {0: 'FREQ', 1: 'TIME', 2: 'ITIM', 3: 'PER'}
+        modeInvert = {val: key for key, val in modeConvert.items()}
+        if mode is not None:
+            mode = modeConvert[mode]
+            yield self.write(':CALC:MARK{:d}:X:READ {:s}'.format(channel, mode))
+        resp = yield self.query(':CALC:MARK{:d}:X:READ?'.format(channel))
+        returnValue(modeInvert[resp])
+
+    @inlineCallbacks
+    def markerTrack(self, channel, status):
+        if status is not None:
+            yield self.write(':CALC:MARK{:d}:TRAC:STAT {:d}'.format(channel, status))
+        resp = yield self.query(':CALC:MARK{:d}:TRAC:STAT?'.format(channel))
+        returnValue(bool(int(resp)))
+
+
+    # MARKER READOUT
+    @inlineCallbacks
+    def markerAmplitude(self, channel):
+        resp = yield self.query(':CALC:MARK{:d}:Y?'.format(channel))
+        returnValue(float(resp))
+
+    @inlineCallbacks
+    def markerFrequency(self, channel, freq):
+        if freq is not None:
+            if (freq > 0) and (freq < 1.5e9):
+                yield self.write(':CALC:MARK{:d}:X {:d}'.format(channel, freq))
+            else:
+                raise Exception('Error: marker frequency must be in range: [0, 1.5e9].')
+        resp = yield self.query(':CALC:MARK{:d}:X?'.format(channel))
+        returnValue(float(resp))
+
+
+    # PEAK
+    @inlineCallbacks
+    def peakSearch(self, status):
+        if status is not None:
+            yield self.write(':CALC:MARK:CPE:STAT {:d}'.format(status))
+        resp = yield self.query(':CALC:MARK:CPE:STAT?')
+        returnValue(bool(int(resp)))
+
+    @inlineCallbacks
+    def peakSet(self, status):
+        # todo:
+        pass
+
+    @inlineCallbacks
+    def peakNext(self, status):
+        # todo:
+        pass
+
+
+    # BANDWIDTH
+    @inlineCallbacks
+    def bandwidthSweepTime(self, time):
+        if time is not None:
+            if (time > 2e-6) and (time < 7500):
+                yield self.write(':SENS:SWE:TIME {:f}'.format(time))
+            else:
+                raise Exception('Error: sweep time must be in range: [2e-6, 7500].')
+        resp = yield self.query(':SENS:SWE:TIME?')
+        returnValue(float(resp))
+
+    @inlineCallbacks
+    def bandwidthResolution(self, bw):
+        if bw is not None:
+            if (bw > 10) and (bw < 1e7):
+                yield self.write(':SENS:BAND:RES {:f}'.format(bw))
+            else:
+                raise Exception('Error: resolution bandwidth must be in range: [10, 1e7].')
+        resp = yield self.query(':SENS:BAND:RES?')
+        returnValue(int(resp))
+
+    @inlineCallbacks
+    def bandwidthVideo(self, bw):
+        if bw is not None:
+            if (bw > 10) and (bw < 1e7):
+                yield self.write(':SENS:BAND:VID {:f}'.format(bw))
+            else:
+                raise Exception('Error: video bandwidth must be in range: [10, 1e7].')
+        resp = yield self.query(':SENS:BAND:VID?')
+        returnValue(int(resp))
+
+
+    # TRACE
+    @inlineCallbacks
+    def getTrace(self, channel):
+        # set data format
+        yield self.write(':FORM:TRAC:DATA ASC')
+
+        # get data
+        data = yield self.query(':TRAC:DATA? TRACE{:d}'.format(channel))
+        data = self._processData(data)
+
+        # create x-axis
+        freq_start = yield self.query(':SENS:FREQ:START?')
+        freq_stop = yield self.query(':SENS:FREQ:STOP?')
+        xAxis = np.linspace(int(freq_start), int(freq_stop), len(data))
+
+        returnValue((xAxis, data))
+
+
+    # HELPER
+    def _processData(self, data):
+        """
+        Process data for header and separate data values.
+        """
+        # header is in #NXXXXXXXXX format
+        tmc_N = int(data[1])
+
+        # remove header and split data
+        processed_data = np.array(data[2 + tmc_N:].split(', '), dtype=float)
+        return processed_data
