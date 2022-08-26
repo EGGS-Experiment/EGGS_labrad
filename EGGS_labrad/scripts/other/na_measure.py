c75de58e7e7a9c45687387a9171a8b6c6c50bdb5
Clayton Ho <claytonho@g.ucla.edu>
2022-08-25 19:58:10 -0700

    added scripts

--- EGGS_labrad/scripts/other/na_measure.py
+++ EGGS_labrad/scripts/other/na_measure.py
@@ -5,33 +5,35 @@ Measure a value from the network analyzer.
 import labrad
 from time import time, sleep
 from datetime import datetime
+import pyvisa
+from EGGS_labrad.clients import createTrunk
 
+rm=pyvisa.ResourceManager()
+lr=rm.list_resources()
+na=rm.open_resource(lr[-1])
 
 cxn = labrad.connect()
-na=cxn.network_analyzer_server
-na.select_device()
-
+#na=cxn.network_analyzer_server
+#na.select_device()
+yz=na.query('*IDN?')
+print('id: {}'.format(yz))
 
 # connect to eggs labrad
 dv = cxn.data_vault
 cr_dv = cxn.context()
 
 # create dataset
-date = datetime.now()
-year = str(date.year)
-month = '{:02d}'.format(date.month)
-trunk1 = '{0:s}_{1:s}_{2:02d}'.format(year, month, date.day)
-trunk2 = '{0:s}_{1:02d}:{2:02d}'.format('Network Analyzer Measure', date.hour, date.minute)
-dv.cd(['', year, month, trunk1, trunk2], True, context=cr_dv)
+trunk = createTrunk('HR Drift Characterization')
+dv.cd(trunk, True, context=cr_dv)
 
 dv.new('HR Drift', [('Elapsed time', 's')], [('Channel 1', 'Signal Min', 'dBm')], context=cr_dv)
-val = na.marker_measure(1)
+val = na.query('CALC1:MARK:FUNC:RES?')
 print(val)
 
 # start recording
 starttime = time()
 while True:
-    val = na.marker_measure(1)
+    val = na.query('CALC1:MARK:FUNC:RES?')
     elapsedtime = time() - starttime
     dv.add(elapsedtime, float(val), context=cr_dv)
     sleep(0.5)
