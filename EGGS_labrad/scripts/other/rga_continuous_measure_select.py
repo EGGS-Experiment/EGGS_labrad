f3676d0c8e543afc145611c4ed813a42f43e9bb4
Clayton Ho <claytonho@g.ucla.edu>
2022-09-26 01:01:05 -0700

    fixed rga continuous measurement, works well

--- EGGS_labrad/scripts/other/rga_continuous_measure_select.py
+++ EGGS_labrad/scripts/other/rga_continuous_measure_select.py
@@ -1,5 +1,4 @@
 # general import
-from numpy import linspace
 from time import time, sleep
 from EGGS_labrad.clients import createTrunk
 
@@ -10,38 +9,31 @@ cxn = labrad.connect()
 # todo: make real script
 
 # parameters
-(mass_initial, mass_final, mass_steps) = (1, 10, 10)
-scan_type = 'h'
+masses = (1, 2, 10, 40)
 
 # get servers
 rga = cxn.rga_server
 dv = cxn.data_vault
 cr = cxn.context()
 
-# set up rga scan
-rga.scan_mass_initial(mass_initial)
-rga.scan_mass_final(mass_final)
-rga.scan_mass_steps(mass_steps)
-num_points = rga.scan_points(scan_type)
+# create independent variable array
+dv_dep_var = [('{:.01f} amu'.format(mass), 'Pressure', 'Torr') for mass in masses]
 
-# create array for independent variables (i.e. mass array)
-dv_dep_var = [('{:.01f} amu'.format(mass), 'Pressure', 'Torr')
-              for mass in linspace(mass_initial, mass_final, num_points)]
-
-# create dataset
+# set up dataset
 trunk_tmp = createTrunk("RGA Continuous Measurement")
 dv.cd(trunk_tmp, True, context=cr)
 dv.new('RGA Continuous Measurement', [('Elapsed time', 't')], dv_dep_var, context=cr)
 
 # prepare to start
 rga.ionizer_filament('*')
+res = [0] * len(masses)
 start_time = time()
 
 # start recording
 while time() - start_time < 3600:
     # take rga data
-    res = rga.scan_start(scan_type, 1)
-    res = list(res[1])
+    for i, mass in enumerate(masses):
+        res[i] = rga.smm_start(mass)
     #print(res)
 
     # add to dataset
