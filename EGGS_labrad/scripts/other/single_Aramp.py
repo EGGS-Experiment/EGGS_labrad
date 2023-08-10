adf3b00d1c53f4797b7e2d7352de0d1e1498a1b6
Clayton Ho <claytonho@g.ucla.edu>
2023-08-10 14:27:36 -0700

    updates to single aramp file

--- EGGS_labrad/scripts/other/single_Aramp.py
+++ EGGS_labrad/scripts/other/single_Aramp.py
@@ -1,3 +1,4 @@
+#Grants's stuff
 import labrad
 import time
 
@@ -6,16 +7,19 @@ servers = {'amo8': 'DC Server'}
 cxn=labrad.connect()
 dc=cxn.dc_server
 #dc.voltage(23,0)
-dc.voltage(24,1)
+#dc.voltage_fast(24,1)
 
 #dc.toggle(23,1)
-dc.toggle(24,1)
+#dc.toggle(24,1)
 #dc.voltage(23,18+.277)
-dc.voltage(24,11.4)
+#dc.voltage_fast(27,1)
+dc.voltage_fast(23,6)
 
-time.sleep(.3)
+time.sleep(.25)
 
 #dc.voltage(23,0)
-dc.voltage(24,1)
+#dc.voltage_fast(27,292)
+dc.voltage_fast(23,0)
 #dc.toggle(23,0)
-dc.toggle(24,1)
+#dc.toggle(27,1)
+dc.toggle(23,1)
