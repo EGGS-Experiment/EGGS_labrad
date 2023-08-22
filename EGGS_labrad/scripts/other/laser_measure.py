5cdc4e8b57d8b4dfba44c4a16b0787eefbb7a7b4
Clayton Ho <claytonho@g.ucla.edu>
2023-08-21 17:41:44 -0700

    saving changes; minor unimportant update to laser measure script

--- EGGS_labrad/scripts/other/laser_measure.py
+++ EGGS_labrad/scripts/other/laser_measure.py
@@ -6,28 +6,39 @@ import labrad
 from time import time, sleep
 from datetime import datetime
 
+from EGGS_labrad.clients import createTrunk
+
+name_tmp = "Wavemeter Frequency Measurement"
+
+
+# connect to eggs labrad
+cxn_eggs = labrad.connect()
+dv = cxn_eggs.data_vault
+cr_dv = cxn_eggs.context()
+
+
 # connect to wavemeter labrad
 cxn_wm = labrad.connect('10.97.111.8', password='lab')
 wm = cxn_wm.multiplexerserver
 cr_wm = cxn_wm.context()
-# connect to eggs labrad
-cxn_eggs = labrad.connect('localhost', password='lab')
-dv = cxn_eggs.data_vault
-cr_dv = cxn_eggs.context()
 
 # create dataset
 date = datetime.now()
 year = str(date.year)
-month = '{:02d}'.format(date.month)
-trunk1 = '{0:s}_{1:s}_{2:02d}'.format(year, month, date.day)
-trunk2 = '{0:s}_{1:02d}:{2:02d}'.format('397 Measure', date.hour, date.minute)
-dv.cd(['', year, month, trunk1, trunk2], True, context=cr_dv)
-dv.new('397nm Power', [('Elapsed time', 's')], [('397', 'Frequency', 'THz')], context=cr_dv)
+trunk_tmp = createTrunk(name_tmp)
+dv.cd(trunk_tmp, True, context=cr_dv)
+dv.new(
+    name_tmp,
+    [('Time', 's')],
+    [('393nm Frequency', 'Frequency', 'THz')],
+    context=cr_dv
+)
+print('Data vault successfully setup.')
 
 # start recording
 starttime = time()
 while True:
-    freq = wm.get_frequency(14)
+    freq = wm.get_frequency(4)
     elapsedtime = time() - starttime
     dv.add(elapsedtime, freq, context=cr_dv)
-    sleep(5)
+    sleep(1)
