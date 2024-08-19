a83f2f6d46fa9cbf89e86f999bb682a578c4bdf2
Clayton Ho <claytonho@g.ucla.edu>
2024-08-18 19:41:58 -0700

    removed interlock functionality from NexTorr server since they were ill-advised and kinda useless/problematic

--- EGGS_labrad/servers/cryovac/niops03_server.py
+++ EGGS_labrad/servers/cryovac/niops03_server.py
@@ -2,7 +2,7 @@
 ### BEGIN NODE INFO
 [info]
 name = NIOPS03 Server
-version = 1.0.0
+version = 1.1.0
 description = Controls NIOPS03 Power Supply which controls the ion pump and getter
 instancename = NIOPS03 Server
 
@@ -34,35 +34,23 @@ class NIOPS03Server(SerialDeviceServer, PollingServer):
     Controls the NIOPS03 power supply for the ion pump and getter.
     """
 
-    name = 'NIOPS03 Server'
-    regKey = 'NIOPS03 Server'
-    serNode = 'MongKok'
-    port = 'COM38'
+    name =      'NIOPS03 Server'
+    regKey =    'NIOPS03 Server'
+    serNode =   'MongKok'
+    port =      'COM38'
 
-    timeout = WithUnit(3.0, 's')
-    baudrate = 115200
+    timeout =   WithUnit(3.0, 's')
+    baudrate =  115200
 
     POLL_ON_STARTUP = True
 
 
     # SIGNALS
-    pressure_update = Signal(999999, 'signal: pressure update', 'v')
-    voltage_update = Signal(999998, 'signal: voltage update', 'i')
-    temperature_update = Signal(999997, 'signal: temperature update', '(vv)')
-    ip_power_update = Signal(999996, 'signal: ip power update', 'b')
-    np_power_update = Signal(999995, 'signal: np power update', 'b')
-
-
-    # STARTUP
-    def initServer(self):
-        # call parent initserver to support further subclassing
-        super().initServer()
-        # interlock
-        self.interlock_active = True
-        self.interlock_pressure = _NI03_MAX_PRESSURE
-        # interlock 2
-        self.interlock2_active = False
-        self.interlock2_pressure = _NI03_MIN_PRESSURE
+    pressure_update =       Signal(999999,  'signal: pressure update', 'v')
+    voltage_update =        Signal(999998,  'signal: voltage update', 'i')
+    temperature_update =    Signal(999997,  'signal: temperature update', '(vv)')
+    ip_power_update =       Signal(999996,  'signal: ip power update', 'b')
+    np_power_update =       Signal(999995,  'signal: np power update', 'b')
 
 
     # STATUS
@@ -244,126 +232,13 @@ class NIOPS03Server(SerialDeviceServer, PollingServer):
         returnValue(temp_list)
 
 
-    # INTERLOCK
-    @setting(311, 'Interlock', status='b', press='v', returns='(bv)')
-    def interlock(self, c, status=None, press=None):
-        """
-        Activates an interlock, switching off the ion pump
-        and getter if pressure exceeds a given value.
-        Pressure is taken from the Twistorr74 turbo pump server.
-        Arguments:
-            status  (bool)  : the interlock status.
-            press   (float) : the maximum pressure (in mbar).
-        Returns:
-                    (bool)  : the interlock status.
-                    (float) :  the maximum pressure (in mbar).
-        """
-        # empty call returns getter
-        if (status is None) and (press is None):
-            return (self.interlock_active, self.interlock_pressure)
-        # ensure pressure is valid
-        if press is None:
-            pass
-        elif (press < 1e-11) or (press > 1e-4):
-            raise Exception('Error: invalid pressure interlock range. Must be between (1e-11, 1e-4) mbar.')
-        else:
-            self.interlock_pressure = press
-        # set interlock parameters
-        self.interlock_active = status
-        return (self.interlock_active, self.interlock_pressure)
-    
-    @setting(312, 'Interlock 2', status='b', press='v', returns='(bv)')
-    def interlock2(self, c, status=None, press=None):
-        """
-        Activates an interlock, switching ON the getter
-        if pressure goes below a given value.
-        Pressure is taken from the Twistorr74 turbo pump server.
-        This is designed to allow completely self-regulating
-        getter activation sequences.
-        Arguments:
-            status  (bool)  : the interlock status.
-            press   (float) : the minimum pressure (in mbar).
-        Returns:
-                    (bool)  : the interlock status.
-                    (float) :  the minimum pressure (in mbar).
-        """
-        # empty call returns getter
-        if (status is None) and (press is None):
-            return (self.interlock2_active, self.interlock2_pressure)
-        # ensure pressure is valid
-        if press is None:
-            pass
-        elif (press < 1e-11) or (press > 1e-4):
-            raise Exception('Error: invalid pressure interlock range. Must be between (1e-11, 1e-4) mbar.')
-        else:
-            self.interlock2_pressure = press
-        # set interlock parameters
-        self.interlock2_active = status
-        return (self.interlock2_active, self.interlock2_pressure)
-
-
     # POLLING
     @inlineCallbacks
     def _poll(self):
         """
         Polls the device for pressure readout and checks the interlock.
         """
-        # check interlock
-        if self.interlock_active:
-            try:
-                # try to get twistorr74 server
-                yield self.client.refresh()
-                tt = yield self.client.twistorr74_server
-                # switch off ion pump if pressure is above a certain value
-                press_tmp = yield tt.pressure()
-                if press_tmp >= self.interlock_pressure:
-                    print('Error: Twistorr74 pressure reads {:.2e} mbar.'.format(press_tmp))
-                    print('\tAbove threshold of {:.2e} mbar for Ion Pump to be on.'.format(self.interlock_pressure))
-                    print('\tSending shutoff signal to ion pump and getter.')
-                    try:
-                        # send shutoff signals; don't use ser.acquire() since
-                        # shutoff needs to happen NOW
-                        yield self.ser.acquire() #tmp remove
-                        yield self.ser.write('B' + TERMINATOR)
-                        yield self.ser.read_line('\r')
-                        yield self.ser.write('BN' + TERMINATOR)
-                        yield self.ser.read_line('\r')
-                        self.ser.release()
-                        # update listeners on power status
-                        self.ip_power_update(False)
-                        self.np_power_update(False)
-                        # disable interlock 2 to minimize overhead once activated
-                        self.interlock2_active = False
-                        # ensure interlock active
-                        self.interlock_active = True
-                    except Exception as e:
-                        print('Error: unable to shut off ion pump and/or getter.')
-                elif (press_tmp <= self.interlock2_pressure) and (self.interlock2_active):
-                    print('Below pressure for getter activation.')
-                    print('\tSending activation signal to getter.')
-                    try:
-                        # set NP activation mode
-                        yield self.ser.acquire()
-                        yield self.ser.write('M1' + TERMINATOR)
-                        yield self.ser.read_line('\r')
-                        # switch on NP
-                        yield self.ser.write('GN' + TERMINATOR)
-                        yield self.ser.read_line('\r')
-                        self.ser.release()
-                        # update listeners on power status
-                        self.np_power_update(True)
-                        # disable interlock 2 to minimize overhead once activated
-                        self.interlock2_active = False
-                        # ensure interlock active
-                        self.interlock_active = True
-                    except Exception as e:
-                        print('Error: unable to activate getter.')
-            except KeyError:
-                print('Warning: Twistorr74 server not available for interlock.')
-            except Exception as e:
-                print('Warning: unable to read pressure from Twistorr74 server.')
-                print('\tSkipping this loop.')
-        # query
+        # query device hardware
         yield self.pressure_ip(None)
         yield self.voltage_ip(None, None)
         yield self.temperature(None)
